4ce408046f023db2a603b217f88a7f0e28ba58a3
tengdahe <tengdahe@192.168.1.5>
2018-10-31 09:26:40 -0700

    add unfinished

---
+++ LeetCodePractice/src/BFS/TheMazeIII.java
@@ -0,0 +1,24 @@
+package BFS;
+
+public class TheMazeIII {
+	class Point {
+		int x, y, dist;
+		String path;
+		public Point(int x, int y, int dist, String path) {
+			this.x = x;
+			this.y = y;
+			this.dist = dist;
+			this.path = path;
+		}
+	}
+	int rows, cols;
+	/**
+     * @param maze: the maze
+     * @param ball: the ball position
+     * @param hole: the hole position
+     * @return: the lexicographically smallest way
+     */
+    public String findShortestWay(int[][] maze, int[] ball, int[] hole) {
+        
+    }
+}
