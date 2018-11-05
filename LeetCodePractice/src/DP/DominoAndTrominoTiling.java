4d906fafb6f9558c4e764c401fc2088d49e2926b
tenh <tenh@TengdaHe-1.redmond.corp.microsoft.com>
2018-11-04 17:02:34 -0800

    add more

---
+++ LeetCodePractice/src/DP/DominoAndTrominoTiling.java
@@ -0,0 +1,35 @@
+package DP;
+
+/*We have two types of tiles: a 2x1 domino shape, and an "L" tromino shape. These shapes may be rotated.
+
+XX  <- domino
+
+XX  <- "L" tromino
+X
+Given N, how many ways are there to tile a 2 x N board? Return your answer modulo 10^9 + 7.
+
+(In a tiling, every square must be covered by a tile. Two tilings are different if and only if there are two 4-directionally adjacent cells on the board such that exactly one of the tilings has both squares occupied by a tile.)
+
+Example:
+Input: 3
+Output: 5
+Explanation: 
+The five different ways are listed below, different letters indicates different tiles:
+XYZ XXZ XYY XXY XYY
+XYZ YYZ XZZ XYY XXY
+https://www.youtube.com/watch?v=S-fUTfqrdq8
+*/
+public class DominoAndTrominoTiling {
+	public int numTilings(int N) {
+		int mod = 1000000007;
+		long[][] dp = new long[N + 1][3];
+		dp[0][0] = 1;
+		dp[1][0] = 1;
+		for(int i = 2; i <= N; i++){
+			dp[i][0] = (dp[i - 1][0] + dp[i - 2][0] + dp[i - 1][1] + dp[i - 1][2]) % mod;
+			dp[i][1] = (dp[i - 1][2] + dp[i - 2][0]) % mod;
+			dp[i][2] = (dp[i - 1][1] + dp[i - 2][0]) % mod;
+		}
+		return (int)dp[N][0];
+	}
+}
