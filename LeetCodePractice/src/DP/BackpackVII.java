32e913b2ad208ae09cb35d8f74726c2b93237c86
tengdahe <tengdahe@192.168.1.5>
2018-11-07 09:09:31 -0800

    add

---
+++ LeetCodePractice/src/DP/BackpackVII.java
@@ -0,0 +1,24 @@
+package DP;
+/*Assume that you have n yuan. There are many kinds of rice in the supermarket. Each kind of rice is bagged and must be purchased in the whole bag. 
+Given the weight, price and quantity of each type of rice, find the maximum weight of rice that you can purchase.
+
+Example
+Given:
+n = 8
+prices = [2,4]
+weight = [100,100]
+amounts = [4,2]
+
+Return:400*/
+public class BackpackVII {
+	/**
+     * @param n: the money of you
+     * @param prices: the price of rice[i]
+     * @param weight: the weight of rice[i]
+     * @param amounts: the amount of rice[i]
+     * @return: the maximum weight
+     */
+    public int backPackVII(int n, int[] prices, int[] weight, int[] amounts) {
+        // write your code here
+    }
+}
---
+++ LeetCodePractice/src/DP/BackpackVIII.java
@@ -0,0 +1,22 @@
+package DP;
+/*Give some coins of different value and their quantity. Find how many values which are in range 1 ~ n can these coins be combined
+
+Example
+Given:
+n = 10
+value = [1,2,4]
+amount = [2,1,1]
+
+Return: 8
+They can combine all the values in 1 ~ 8*/
+public class BackpackVIII {
+	/**
+     * @param n: the value from 1 - n
+     * @param value: the value of coins
+     * @param amount: the number of coins
+     * @return: how many different value
+     */
+    public int backPackVIII(int n, int[] value, int[] amount) {
+        // write your code here
+    }
+}
