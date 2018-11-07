d08b3c6b7221cfefa7d14130e280e976bb129cce
tengdahe <tengdahe@192.168.1.5>
2018-11-07 09:04:51 -0800

    add

--- LeetCodePractice/src/DP/Backpack.java
+++ LeetCodePractice/src/DP/Backpack.java
@@ -1,5 +1,10 @@
 package DP;
-//Given n items with size Ai, an integer m denotes the size of a backpack. How full you can fill this backpack?
+/*Given n items with size Ai and value Vi, and a backpack with size m. 
+What's the maximum value can you put into the backpack?
+Example
+If we have 4 items with size [2, 3, 5, 7], the backpack size is 11, we can select [2, 3, 5], so that the max size we can fill this backpack is 10. If the backpack size is 12. we can select [2, 3, 7] so that we can fulfill the backpack.
+
+You function should return the max size we can fill in the given backpack.*/
 public class Backpack {
 	/**
      * @param m: An integer m denotes the size of a backpack
