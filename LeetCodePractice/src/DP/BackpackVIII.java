e225c3c629ae80bee7b49ecaeed669c19afcc58b
tengdahe <tengdahe@192.168.1.5>
2018-11-09 09:34:43 -0800

    add

--- LeetCodePractice/src/DP/BackpackVIII.java
+++ LeetCodePractice/src/DP/BackpackVIII.java
@@ -18,6 +18,18 @@ public class BackpackVIII {
      */
     public int backPackVIII(int n, int[] value, int[] amount) {
         // write your code here
-    	
+    	int len = value.length;
+    	int[][] dp = new int[len + 1][n + 1];
+    	for(int i = 0; i <= len; i ++){
+    		for(int j = 1; j <= n; j++){
+    			for(int a = 0; a <= amount[i]; a++){
+    				dp[i][j] = Math.max(dp[i][j], dp[i - 1][j]);
+    				if(j >= a * value[i - 1]){
+    					
+    				}
+    				dp[i][j] = dp[i - 1][j] + 
+    			}
+    		}
+    	}
     }
 }
