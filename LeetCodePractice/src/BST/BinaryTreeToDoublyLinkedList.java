e76bac07226cd222011e0336d235072f5438f41f
tengdahe <tengdahe@192.168.1.5>
2018-11-16 23:35:06 -0800

    finish

--- LeetCodePractice/src/BST/BinaryTreeToDoublyLinkedList.java
+++ LeetCodePractice/src/BST/BinaryTreeToDoublyLinkedList.java
@@ -10,19 +10,29 @@ public class BinaryTreeToDoublyLinkedList {
 			val = x;
 		}
 	}
-	class DoublyNode {
-		DoublyNode pre;
-		DoublyNode next;
-		int val;
-		
-		DoublyNode(int x) {
-			val = x;
+	
+	TreeNode head = null;
+	static TreeNode pre = null;
+
+	public void printList(){
+		while(head != null){
+			System.out.println(head.val + " ");
+			head = head.right;
 		}
 	}
-	public DoublyNode convertToList(TreeNode root){
+
+	public void convertToList(TreeNode root){
 		if(root == null){
-			return null;
+			return;
 		}
-		
+		convertToList(root.left);
+		if(pre == null){
+			head = root;
+		} else {
+			pre.right = root;
+			root.left =pre;
+		}
+		pre = root;
+		convertToList(root.right);
 	}
 }
