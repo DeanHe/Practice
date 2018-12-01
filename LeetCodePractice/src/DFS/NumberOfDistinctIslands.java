969097da067bce707817cf13f92089ba7dda5954
tenh <tenh@MININT-VID483M.redmond.corp.microsoft.com>
2018-11-30 20:47:48 -0800

    add 11/30

---
+++ LeetCodePractice/src/DFS/NumberOfDistinctIslands.java
@@ -0,0 +1,43 @@
+package DFS;
+/*Given a non-empty 2D array grid of 0's and 1's, an island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+
+Count the number of distinct islands. An island is considered to be the same as another if and only if one island can be translated (and not rotated or reflected) to equal the other.
+
+Notice that:
+
+11
+1
+and
+
+ 1
+11
+are considered different island shapes, because we do not consider reflection / rotation.
+
+Example
+Given grid = 
+[
+[1,1,0,0,0],
+[1,1,0,0,0],
+[0,0,0,1,1],
+[0,0,0,1,1]
+]
+return 1
+Given grid = 
+[
+[1,1,0,1,1],
+[1,0,0,0,0],
+[0,0,0,0,1],
+[1,1,0,1,1]
+]
+return 3
+Notice
+The length of each dimension in the given grid does not exceed 50.*/
+public class NumberOfDistinctIslands {
+	/**
+     * @param grid: a list of lists of integers
+     * @return: return an integer, denote the number of distinct islands
+     */
+    public int numberofDistinctIslands(int[][] grid) {
+        // write your code here
+    }
+}
