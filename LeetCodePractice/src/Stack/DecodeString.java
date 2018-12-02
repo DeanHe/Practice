83231c61b9d45fa44a0fe9f94bd9ffc71e9ec33b
tenh <tenh@MININT-VID483M.redmond.corp.microsoft.com>
2018-12-01 17:50:58 -0800

    add 12/1/2018

---
+++ LeetCodePractice/src/Stack/DecodeString.java
@@ -0,0 +1,71 @@
+package Stack;
+
+import java.lang.annotation.Repeatable;
+import java.util.Stack;
+
+/*Given an expression s includes numbers, letters and brackets. Number represents the number of repetitions inside the brackets(can be a string or another expression)£®Please expand expression to be a string.
+
+Example
+s = abc3[a] return abcaaa
+s = 3[abc] return abcabcabc
+s = 4[ac]dy, return acacacacdy
+s = 3[2[ad]3[pf]]xyz, return adadpfpfpfadadpfpfpfadadpfpfpfxyz*/
+public class DecodeString {
+	/**
+     * @param s: an expression includes numbers, letters and brackets
+     * @return: a string
+     */
+	public String expressionExpand(String s) {
+        // write your code here
+        char[] arr = s.toCharArray();
+    	Stack<String> stack = new Stack<>();
+    	int count = 0;
+    	for(char c : arr) {
+    		if(c == '[') {
+    			stack.push(String.valueOf(count));
+    			count = 0;
+    		} else if(c == ']'){
+    			String repeat = getRepeat(stack);
+    			stack.push(repeat);
+    		} else if(Character.isDigit(c)){
+    			count = count * 10 + c - '0';
+    		} else {
+    			stack.push(String.valueOf(c));
+    		}
+    	}
+    	return getRepeat(stack);
+    }
+    private String getRepeat(Stack<String> stack) {
+    	StringBuilder sb = new StringBuilder();
+    	int count = -1;
+    	while(!stack.isEmpty()) {
+    		if(isNumber(stack.peek())) {
+    			count = Integer.parseInt(stack.pop());
+    			break;
+    		} else {
+    			sb.insert(0, stack.pop());
+    		}
+    	}
+    	StringBuilder ans = new StringBuilder();
+    	if(count == -1){
+    	    ans.append(sb.toString());
+    	} else {
+    	    for(int i = 0; i < count; i++) {
+    		    ans.append(sb.toString());
+    	    }
+    	}
+    	return ans.toString();
+    }
+    private boolean isNumber(String s) {
+    	if(s.isEmpty()) {
+    		return false;
+    	}
+    	char[] arr = s.toCharArray();
+    	for(char c : arr) {
+    		if(!Character.isDigit(c)) {
+    			return false;
+    		}
+    	}
+    	return true;
+    }
+}
---
+++ LeetCodePractice/src/Stack/EvaluateReversePolishNotation.java
@@ -0,0 +1,38 @@
+package Stack;
+
+import java.util.*;
+
+/*Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+Valid operators are +, -, *, /. Each operand may be an integer or another expression.
+http://en.wikipedia.org/wiki/Reverse_Polish_notation
+Example
+["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
+["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6*/
+public class EvaluateReversePolishNotation {
+	/**
+	 * @param tokens The Reverse Polish Notation
+	 * @return the value
+	 */
+	public int evalRPN(String[] tokens) {
+		String operators = "+-*/";
+		Stack<Integer> stack = new Stack<>();
+		for (String s : tokens) {
+			if (!operators.contains(s)) {
+				stack.push(Integer.valueOf(s));
+			} else {
+				int b = stack.pop();
+				int a = stack.pop();
+				if (s.equals("+")) {
+					stack.push(a + b);
+				} else if (s.equals("-")) {
+					stack.push(a - b);
+				} else if (s.equals("*")) {
+					stack.push(a * b);
+				} else if (s.equals("/")) {
+					stack.push(a / b);
+				}
+			}
+		}
+		return stack.pop();
+	}
+}
