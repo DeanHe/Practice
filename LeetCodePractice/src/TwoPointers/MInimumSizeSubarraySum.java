c00275835b56d4406a9de73b2f0eb476d644b4b3
tenh <tenh@TengdaHe-1.redmond.corp.microsoft.com>
2018-12-24 18:09:55 -0800

    add 12/24

--- LeetCodePractice/src/TwoPointers/MInimumSizeSubarraySum.java
+++ LeetCodePractice/src/TwoPointers/MInimumSizeSubarraySum.java
@@ -1,5 +1,5 @@
 package TwoPointers;
-/*Given an array of n positive integers and a positive integer s, find the minimal length of a contiguous subarray of which the sum ≥ s. If there isn't one, return 0 instead.
+/*Given an array of n positive integers and a positive integer s, find the minimal length of a contiguous subarray of which the sum >= s. If there isn't one, return 0 instead.
 
 Example: 
 
