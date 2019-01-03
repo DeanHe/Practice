0f81bdaaf95253519c8e5f63b970e151d6da173f
tengdahe <tengdahe@192.168.1.5>
2019-01-03 09:44:10 -0800

    add 1/3

--- LeetCodePractice/src/SweepLine/MeetingRoomsII.java
+++ LeetCodePractice/src/SweepLine/MeetingRoomsII.java
@@ -1,10 +1,37 @@
 package SweepLine;
 // https://www.programcreek.com/2014/05/leetcode-meeting-rooms-ii-java/
-/*Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), find the minimum number of conference rooms required.
+/*Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), 
+ * find the minimum number of conference rooms required.
 
 Example
 Given intervals = [(0,30),(5,10),(15,20)], return 2.*/
 import java.util.*;
+
+class Point {
+	int time;
+	boolean isStart;
+	public Point(int time, boolean isStart){
+		this.time = time;
+		this.isStart = isStart;
+	}
+	public static Comparator<Point> PointComparator = new Comparator<Point>() {
+		
+		@Override
+		public int compare(Point o1, Point o2) {
+			if(o1.time == o2.time){
+				if((o1.isStart && o2.isStart) || (!o1.isStart && !o2.isStart)){
+					return 0;
+				} else if(o1.isStart){
+					return 1;
+				} else {
+					return -1;
+				}
+			} else {
+				return o1.time - o2.time;
+			}
+		}
+	};
+}
 
 public class MeetingRoomsII {
 	class Interval {
@@ -15,29 +42,22 @@ public class MeetingRoomsII {
 		}
 	}
 	public int minMeetingRooms(Interval[] intervals) {
-	    if(intervals==null||intervals.length==0)
-	        return 0;
-	 
-	    Arrays.sort(intervals, new Comparator<Interval>(){
-	        public int compare(Interval i1, Interval i2){
-	            return i1.start-i2.start;
-	        }
-	    });
-	    // use a pq to get the swept interval has min end value
-	    PriorityQueue<Integer> queue = new PriorityQueue<Integer>();
-	    int count=1;
-	    queue.offer(intervals[0].end);
-	 
-	    for(int i=1; i<intervals.length; i++){
-	    	Interval cur = intervals[i];
-	        if(cur.start < queue.peek()){
-	            count++;	 
-	        }else{
-	            queue.poll();
-	        } 
-	        queue.offer(cur.end);
-	    }	 
-	    return count;
+		List<Point> list = new ArrayList<>(intervals.length*2);
+	    for(Interval i : intervals){
+	    	list.add(new Point(i.start, true));
+	    	list.add(new Point(i.end, false));
+	    }
+	    Collections.sort(list, Point.PointComparator);
+	    int count = 0, ans = 0;
+	    for(Point p : list){
+	    	if(p.isStart){
+	    		count++;
+	    	} else {
+	    		count--;
+	    	}
+	    	ans = Math.max(ans, count);
+	    }
+	    return ans;
 	}
 
 }
