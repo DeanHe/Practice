f78d3120ad501867601ec5ad378b8e113e196c09
tenh <tenh@TengdaHe-1.redmond.corp.microsoft.com>
2019-01-15 16:53:17 -0800

    add 1/15 2

---
+++ LeetCodePractice/src/SweepLine/InsertInterval.java
@@ -0,0 +1,44 @@
+package SweepLine;
+
+import java.util.*;
+/*Given a non-overlapping interval list which is sorted by start point.
+
+Insert a new interval into it, make sure the list is still in order and non-overlapping (merge intervals if necessary).
+
+Example
+Insert (2, 5) into [(1,2), (5,9)], we get [(1,9)].
+
+Insert (3, 4) into [(1,2), (5,9)], we get [(1,2), (3,4), (5,9)].
+*/
+public class InsertInterval {
+	class Interval {
+	      int start;
+	      int end;
+	      Interval() { start = 0; end = 0; }
+	      Interval(int s, int e) { start = s; end = e; }
+	  }
+	/**
+     * Insert newInterval into intervals.
+     * @param intervals: Sorted interval list.
+     * @param newInterval: A new interval.
+     * @return: A new sorted interval list.
+     */
+    public ArrayList<Interval> insert(ArrayList<Interval> intervals, Interval newInterval) {
+        ArrayList<Interval> result = new ArrayList<Interval>();
+        int insertPos = 0;
+        for(Interval i : intervals){
+            if(i.end < newInterval.start){
+                result.add(i);
+                insertPos++;
+            } else if(i.start > newInterval.end){
+                result.add(i);
+            } else {
+                newInterval.start = Math.min(i.start, newInterval.start);
+                newInterval.end = Math.max(i.end, newInterval.end);
+            }
+        }
+        result.add(insertPos, newInterval);
+        // write your code here
+        return result;
+    }
+}
