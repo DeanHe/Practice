6d039fbb16b0d3737294d793228ec37dbf33ad87
tenh <tenh@TengdaHe-1.redmond.corp.microsoft.com>
2019-01-20 14:16:21 -0800

    add 1/20

--- LeetCodePractice/src/DFS/WordBreakII.java
+++ LeetCodePractice/src/DFS/WordBreakII.java
@@ -1,5 +1,13 @@
 package DFS;
+/*Given a string s and a dictionary of words dict, add spaces in s to construct a sentence where each word is a valid dictionary word.
 
+Return all such possible sentences.
+
+Example
+Gieve s = lintcode,
+dict = ["de", "ding", "co", "code", "lint"].
+
+A solution is ["lint code", "lint co de"].*/
 import java.util.*;
 
 public class WordBreakII {
--- LeetCodePractice/src/DP/WordBreakIII.java
+++ LeetCodePractice/src/DP/WordBreakIII.java
@@ -1,5 +1,18 @@
 package DP;
+/*Give a dictionary of words and a sentence with all whitespace removed, return the number of sentences you can form by inserting whitespaces to the sentence so that each word can be found in the dictionary.
 
+Example
+Given a String CatMat
+Given a dictionary ["Cat", "Mat", "Ca", "tM", "at", "C", "Dog", "og", "Do"]
+return 3
+
+we can form 3 sentences, as follows:
+CatMat = Cat Mat
+CatMat = Ca tM at
+CatMat = C at Mat
+
+Notice
+Ignore case*/
 import java.util.*;
 
 public class WordBreakIII {
