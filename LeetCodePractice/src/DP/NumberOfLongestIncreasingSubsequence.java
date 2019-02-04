6c92eff0ee9840ada3f992c7c9979c0056bc4734
tenh <tenh@TengdaHe-1.redmond.corp.microsoft.com>
2019-02-03 22:23:43 -0800

    add 2/3

---
+++ LeetCodePractice/src/DP/NumberOfLongestIncreasingSubsequence.java
@@ -0,0 +1,45 @@
+package DP;
+/*Given an unsorted array of integers, find the number of longest increasing subsequence.
+
+Example 1:
+Input: [1,3,5,4,7]
+Output: 2
+Explanation: The two longest increasing subsequence are [1, 3, 4, 7] and [1, 3, 5, 7].
+Example 2:
+Input: [2,2,2,2,2]
+Output: 5
+Explanation: The length of longest continuous increasing subsequence is 1, and there are 5 subsequences' length is 1, so output 5.
+Note: Length of the given array will be not exceed 2000 and the answer is guaranteed to be fit in 32-bit signed int.*/
+public class NumberOfLongestIncreasingSubsequence {
+	public int findNumberOfLIS(int[] nums) {
+        int len = nums.length;
+        int[] dp_longest = new int[len];
+        int[] dp_count = new int[len];
+        for(int i = 0; i < len; i++){
+        	dp_count[i] = 1;
+        }
+        for(int i = 0; i < len; i++){
+        	for(int j = 0; j < i; j++){
+        		if(nums[i] > nums[j]){
+        			if(dp_longest[i] == dp_longest[j] + 1){
+        				dp_count[i] += dp_count[j];
+        			} else if(dp_longest[i] < dp_longest[j] + 1){
+        				dp_count[i] = dp_count[j];
+        				dp_longest[i] = dp_longest[j] + 1;
+        			}
+        		}
+        	}
+        }
+        int longest = 0;
+        for(int l : dp_longest){
+        	longest = Math.max(longest, l);
+        }
+        int res = 0;
+        for(int i = 0; i < len; i++){
+        	if(dp_longest[i] == longest){
+        		res += dp_count[i];
+        	}
+        }
+        return res;
+    }
+}
--- LeetCodePractice/src/DP/WildcardMatching.java
+++ LeetCodePractice/src/DP/WildcardMatching.java
@@ -1,43 +1,140 @@
 package DP;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/*Given an input string (s) and a pattern (p), implement wildcard pattern matching with support for '?' and '*'.
+
+'?' Matches any single character.
+'*' Matches any sequence of characters (including the empty sequence).
+The matching should cover the entire input string (not partial).
+
+Note:
+
+s could be empty and contains only lowercase letters a-z.
+p could be empty and contains only lowercase letters a-z, and characters like ? or *.
+Example 1:
+
+Input:
+s = "aa"
+p = "a"
+Output: false
+Explanation: "a" does not match the entire string "aa".
+Example 2:
+
+Input:
+s = "aa"
+p = "*"
+Output: true
+Explanation: '*' matches any sequence.
+Example 3:
+
+Input:
+s = "cb"
+p = "?a"
+Output: false
+Explanation: '?' matches 'c', but the second letter is 'a', which does not match 'b'.
+Example 4:
+
+Input:
+s = "adceb"
+p = "*a*b"
+Output: true
+Explanation: The first '*' matches the empty sequence, while the second '*' matches the substring "dce".
+Example 5:
+
+Input:
+s = "acdcb"
+p = "a*c?b"
+Output: false*/
 public class WildcardMatching {
 	/**
-     * @param s: A string 
-     * @param p: A string includes "?" and "*"
-     * @return: A boolean
-     */
-    public boolean isMatch(String s, String p) {
-        // write your code here
-        int sLen = s.length();
-        int pLen = p.length();
-        //where dp[i][j] means the first i characters in string s matches the first characters of string p. 
-        boolean[][] dp = new boolean[sLen + 1][pLen + 1];
-        //init
-        dp[0][0] = true;
-        for(int j = 1; j <= pLen; j++){
-            if(p.charAt(j - 1) == '*'){
-                dp[0][j] = dp[0][j - 1];    
-            }
-        }
-        /*
-        Transit function: 
-        -- If p.charAt(j - 1) != '*', then dp[i][j] = dp[i - 1][j - 1] IFF s.charAt(i) == p.charAt(j) || p.charAt(j) == '?'
-        -- If p.charAt(j - 1) == '*', then 
-             -- dp[i][j] = dp[i - 1][j - 1] || // Match 1 character
-                           = dp[i][j - 1] || // Match 0 character
-                           = dp[i - 1][j] // Match any sequence of characters 
-        */
-        for(int i = 1; i <= sLen; i++){
-            for(int j = 1; j <= pLen; j++){
-                if(p.charAt(j - 1) != '*'){
-                    if(p.charAt(j - 1) == '?' || s.charAt(i - 1) == p.charAt(j - 1)){
-                    dp[i][j] = dp[i - 1][j - 1];
-                    }
-                } else {
-                    dp[i][j] = dp[i - 1][j - 1] || dp[i - 1][j] || dp[i][j - 1]; 
-                }
-            }
-        }
-        return dp[sLen][pLen];
-     }
+	 * @param s:
+	 *            A string
+	 * @param p:
+	 *            A string includes "?" and "*"
+	 * @return: A boolean
+	 */
+	public boolean isMatch(String s, String p) {
+		// write your code here
+		int sLen = s.length();
+		int pLen = p.length();
+		// where dp[i][j] means the first i characters in string s matches the
+		// first characters of string p.
+		boolean[][] dp = new boolean[sLen + 1][pLen + 1];
+		// init
+		dp[0][0] = true;
+		for (int j = 1; j <= pLen; j++) {
+			if (p.charAt(j - 1) == '*') {
+				dp[0][j] = dp[0][j - 1];
+			}
+		}
+		/*
+		 * Transit function: -- If p.charAt(j - 1) != '*', then dp[i][j] = dp[i
+		 * - 1][j - 1] IFF s.charAt(i) == p.charAt(j) || p.charAt(j) == '?' --
+		 * If p.charAt(j - 1) == '*', then -- dp[i][j] = dp[i - 1][j - 1] || //
+		 * Match 1 character = dp[i][j - 1] || // Match 0 character = dp[i -
+		 * 1][j] // Match any sequence of characters
+		 */
+		for (int i = 1; i <= sLen; i++) {
+			for (int j = 1; j <= pLen; j++) {
+				if (p.charAt(j - 1) != '*') {
+					if (p.charAt(j - 1) == '?' || s.charAt(i - 1) == p.charAt(j - 1)) {
+						dp[i][j] = dp[i - 1][j - 1];
+					}
+				} else {
+					dp[i][j] = dp[i - 1][j - 1] || dp[i - 1][j] || dp[i][j - 1];
+				}
+			}
+		}
+		return dp[sLen][pLen];
+	}
+
+	// with O(1) space, and save star matched substrings
+	public boolean comparison(String str, String pattern) {
+		int s = 0, p = 0, match = 0, starIdx = -1;
+		List<String> starMatch = new ArrayList<>();
+		StringBuilder temp = new StringBuilder();
+		while (s < str.length()) {
+			// advancing both pointers
+			if (p < pattern.length() && (pattern.charAt(p) == '?' || str.charAt(s) == pattern.charAt(p))) {
+				if (temp.length() != 0) {
+					starMatch.add(temp.toString());
+					temp.setLength(0);
+				}
+				s++;
+				p++;
+			}
+			// * found, only advancing pattern pointer
+			else if (p < pattern.length() && pattern.charAt(p) == '*') {
+				starIdx = p;
+				match = s;
+				temp.append(str.charAt(s));
+				p++;
+			}
+			// last pattern pointer was *, advancing string pointer
+			else if (starIdx != -1) {
+				p = starIdx + 1;
+				match++;
+				s = match;
+				if (s < str.length() && str.charAt(s) != pattern.charAt(p)) {
+					temp.append(str.charAt(s));
+				}
+			}
+			// current pattern pointer is not star, last patter pointer was not
+			// *
+			// characters do not match
+			else
+				return false;
+		}
+		if (temp.length() != 0) {
+			starMatch.add(temp.toString());
+			temp.setLength(0);
+		}
+		// check for remaining characters in pattern
+		while (p < pattern.length() && pattern.charAt(p) == '*')
+			p++;
+
+		return p == pattern.length();
+	}
 }
