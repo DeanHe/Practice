0d36c9d2437c5271923e765a4088eda9e9d95f98
tenh <tenh@TengdaHe-1.redmond.corp.microsoft.com>
2019-02-12 19:17:02 -0800

    add 2/12

--- LeetCodePractice/src/DP/BackpackVI.java
+++ LeetCodePractice/src/DP/BackpackVI.java
@@ -3,6 +3,31 @@ package DP;
 //Given an integer array nums with all positive numbers and no duplicates, 
 //find the number of possible combinations that add up to a positive integer target.
 // each number can be used unlimited times
+/*Given an integer array with all positive numbers and no duplicates, find the number of possible combinations that add up to a positive integer target.
+
+Example:
+
+nums = [1, 2, 3]
+target = 4
+
+The possible combination ways are:
+(1, 1, 1, 1)
+(1, 1, 2)
+(1, 2, 1)
+(1, 3)
+(2, 1, 1)
+(2, 2)
+(3, 1)
+
+Note that different sequences are counted as different combinations.
+
+Therefore the output is 7.
+ 
+
+Follow up:
+What if negative numbers are allowed in the given array?
+How does it change the problem?
+What limitation we need to add to the question to allow negative numbers?*/
 public class BackpackVI {
 	/**
      * @param nums an integer array and all positive numbers, no duplicates
