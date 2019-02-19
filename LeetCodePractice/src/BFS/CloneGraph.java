61e113de8bf37daa5174ef0c5d071d74f83c3add
tengdahe <tengdahe@192.168.1.5>
2019-02-19 09:38:46 -0800

    add 2/19

---
+++ LeetCodePractice/src/BFS/CloneGraph.java
@@ -0,0 +1,64 @@
+package BFS;
+
+import java.util.*;
+
+/*Clone an undirected graph. Each node in the graph contains a label and a list of its neighbors.
+How we serialize an undirected graph:http://www.lintcode.com/help/graph/
+Nodes are labeled uniquely.
+We use # as a separator for each node, and , as a separator for node label and each neighbor of the node.
+As an example, consider the serialized graph {0,1,2#1,2#2,2}.
+The graph has a total of three nodes, and therefore contains three parts as separated by #.
+First node is labeled as 0. Connect node 0 to both nodes 1 and 2.
+Second node is labeled as 1. Connect node 1 to node 2.
+Third node is labeled as 2. Connect node 2 to node 2 (itself), thus forming a self-cycle.
+Visually, the graph looks like the following:
+
+   1
+  / \
+ /   \
+0 --- 2
+     / \
+     \_/
+Example
+return a deep copied graph.*/
+public class CloneGraph {
+	class UndirectedGraphNode {
+		  int label;
+		  ArrayList<UndirectedGraphNode> neighbors;
+		  UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
+	 };
+	/*
+     * @param node: A undirected graph node
+     * @return: A undirected graph node
+     */
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        // write your code here
+    	if(node == null){
+    		return null;
+    	}
+    	UndirectedGraphNode root_copy = new UndirectedGraphNode(node.label);
+    	// map between original node and copied node
+    	Map<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<>();
+    	map.put(node, root_copy);
+    	Queue<UndirectedGraphNode> queue = new LinkedList<>();
+    	queue.offer(node);
+    	while (!queue.isEmpty()) {
+			UndirectedGraphNode cur = queue.poll();
+			List<UndirectedGraphNode> cur_nbs = cur.neighbors;
+			for(UndirectedGraphNode cur_nb : cur_nbs){
+				if(!map.containsKey(cur_nb)){
+					UndirectedGraphNode cur_nb_copy = new UndirectedGraphNode(cur_nb.label);
+					map.put(cur_nb, cur_nb_copy);
+					UndirectedGraphNode cur_copy = map.get(cur);
+					cur_copy.neighbors.add(cur_nb_copy);
+					queue.offer(cur_nb);
+				} else {
+					UndirectedGraphNode cur_copy = map.get(cur);
+					UndirectedGraphNode cur_nb_copy = map.get(cur_nb);
+					cur_copy.neighbors.add(cur_nb_copy);
+				}
+			}
+		}
+    	return root_copy;
+    }
+}
---
+++ LeetCodePractice/src/Sort/KthLargestElement.java
@@ -0,0 +1,68 @@
+package Sort;
+/*ind K-th largest element in an array.
+
+Example
+Example 1:
+
+Input:
+n = 1, nums = [1,3,4,2]
+Output:
+4
+Example 2:
+
+Input:
+n = 3, nums = [9,3,2,4,8]
+Output:
+4
+Challenge
+O(n) time, O(1) extra memory.
+
+Notice
+You can swap elements in the array*/
+public class KthLargestElement {
+	/**
+     * @param n: An integer
+     * @param nums: An array
+     * @return: the Kth largest element
+     */
+    public int kthLargestElement(int k, int[] nums) {
+        // write your code here
+    	if(nums == null || nums.length == 0){
+    		return 0;
+    	}
+    	if(k <= 0){
+    		return 0;
+    	}
+    	return getKth(nums, 0, nums.length - 1, nums.length - k);
+    }
+    private int getKth(int[] nums, int start, int end, int k){
+    	if(start == end) {
+    		return nums[start];
+    	}
+    	int index = partition(nums, start, end);
+    	if(index == k){
+    		return nums[index];
+    	} else if(index < k){
+    		return getKth(nums, index + 1, end, k);
+    	} else {
+    		return getKth(nums, start, index - 1, k);
+    	}
+    }
+    private int partition(int[] nums, int start, int end){
+    	int pivot = nums[end];
+    	int i = start;
+    	for(int j = start; j < end; j++){
+    		if(nums[j] <= pivot){
+    			swap(nums, i++, j);
+    		}
+    	}
+    	swap(nums, i, end);
+		return i;
+    	
+    }
+    private void swap(int[] nums, int i, int j){
+    	int temp = nums[i];
+    	nums[i] = nums[j];
+    	nums[j] = temp;
+    }
+}
