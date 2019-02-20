312df52faf8a5fe5df18b45d5464fcb55fa21fc4
tengdahe <tengdahe@192.168.1.5>
2019-02-20 09:54:00 -0800

    add 2/20

--- LeetCodePractice/src/DP/BackpackIV.java
+++ LeetCodePractice/src/DP/BackpackIV.java
@@ -17,17 +17,15 @@ public class BackpackIV {
      * @return an integer
      */
     public int backPackIV2D(int[] nums, int target) {
-        int len = nums.length;
-        int dp[][] = new int[len + 1][target + 1];
-        
+    	int len = nums.length;
+        int dp[][] = new int[len + 1][target + 1];       
         dp[0][0] = 1;
         for (int i = 1; i <= len; i++) {
             for (int j = 0; j <= target; j++) {
-                int k = 0; 
-                while(k * nums[i - 1] <= j) {
-                    dp[i][j] += dp[i - 1][j - nums[i - 1] * k];
-                    k+=1;
-                }
+            	dp[i][j] = dp[i - 1][j];
+            	if(j >= nums[i - 1]){
+            		dp[i][j] += dp[i][j - nums[i - 1]];
+            	}
             }
         }  
         return dp[len][target];
