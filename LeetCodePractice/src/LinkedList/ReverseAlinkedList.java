f6e8e6a1cdf2331d7a64279c9b12afd4a283652f
tengdahe <tengdahe@192.168.1.5>
2019-02-20 00:09:49 -0800

    modify one 2/20

--- LeetCodePractice/src/LinkedList/ReverseAlinkedList.java
+++ LeetCodePractice/src/LinkedList/ReverseAlinkedList.java
@@ -1,5 +1,13 @@
 package LinkedList;
+/*Reverse a singly linked list.
 
+Example:
+
+Input: 1->2->3->4->5->NULL
+Output: 5->4->3->2->1->NULL
+Follow up:
+
+A linked list can be reversed either iteratively or recursively. Could you implement both?*/
 public class ReverseAlinkedList {
 	
 	public ListNode reverse(ListNode head){
