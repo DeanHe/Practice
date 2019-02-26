102a00eeb61c0248f5a4aa812ad4d1961f63c000
tenh <tenh@TengdaHe-1.redmond.corp.microsoft.com>
2019-02-25 18:35:18 -0800

    add 2/25

---
+++ LeetCodePractice/src/BFS/WallsAndGates.java
@@ -0,0 +1,77 @@
+package BFS;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*You are given a m x n 2D grid initialized with these three possible values.
+
+-1 - A wall or an obstacle.
+0 - A gate.
+INF - Infinity means an empty room. We use the value 2^31 - 1 = 2147483647 to represent INF as you may assume that the distance to a gate is less than 2147483647.
+Fill each empty room with the distance to its nearest gate. If it is impossible to reach a ROOM, that room should remain filled with INF
+
+Example
+Example1
+
+Input:
+[[2147483647,-1,0,2147483647],[2147483647,2147483647,2147483647,-1],[2147483647,-1,2147483647,-1],[0,-1,2147483647,2147483647]]
+Output:
+[[3,-1,0,1],[2,2,1,-1],[1,-1,2,-1],[0,-1,3,4]]
+
+Explanation:
+the 2D grid is:
+INF  -1  0  INF
+INF INF INF  -1
+INF  -1 INF  -1
+  0  -1 INF INF
+the answer is:
+  3  -1   0   1
+  2   2   1  -1
+  1  -1   2  -1
+  0  -1   3   4
+Example2
+
+Input:
+[[0,-1],[2147483647,2147483647]]
+Output:
+[[0,-1],[1,2]]*/
+public class WallsAndGates {
+	/**
+     * @param rooms: m x n 2D grid
+     * @return: nothing
+     */
+	class Coordinate {
+		int r, c;
+		public Coordinate(int r, int c){
+			this.r = c;
+			this.r = c;
+		}
+	}
+    public void wallsAndGates(int[][] rooms) {
+        // write your code here
+    	int rows = rooms.length;
+    	int cols = rooms[0].length;
+    	Queue<Coordinate> queue = new LinkedList<>();
+    	for(int r = 0; r < rows; r++){
+    		for(int c = 0; c < cols; c++){
+    			if(rooms[r][c] == 0){
+    				queue.offer(new Coordinate(r, c));
+    			}
+    		}
+    	}
+    	int[] direct = new int[]{0, -1, 0, 1, 0};
+    	while(!queue.isEmpty()){
+    		int qLen = queue.size();
+    		for(int i = 0; i < qLen; i++){
+    			Coordinate cur = queue.poll();
+    			for(int j = 0; j < direct.length - 1; j++){
+    				int nb_r = cur.r + direct[j];
+    				int nb_c = cur.c + direct[j + 1];
+    				if(nb_r >= 0 && nb_r < rows && nb_c >= 0 && nb)){
+    					
+    				}
+    			}
+    		}
+    	}
+    }
+}
