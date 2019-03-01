415d14143e8a262ba3096ee1a7d2fd7c08815f9e
tengdahe <tengdahe@192.168.1.5>
2019-03-01 11:04:17 -0800

    add 3/1

--- LeetCodePractice/src/HashMap/GroupShiftedStrings.java
+++ LeetCodePractice/src/HashMap/GroupShiftedStrings.java
@@ -12,6 +12,34 @@ Given a list of strings which contains only lowercase alphabets, group all strin
 ]*/
 public class GroupShiftedStrings {
 	public List<List<String>> groupStrings(String[] strings) {
-		
+		List<List<String>> res = new ArrayList<>();
+		HashMap<String, List<String>> map = new HashMap<>();
+		for(String s : strings){
+			String key = shiftBacktoStart(s);
+			if(!map.containsKey(key)){
+				map.put(key, new ArrayList<>());
+			}
+			map.get(key).add(s);
+		}
+		for(List<String> list : map.values()){
+			Collections.sort(list);
+			res.add(list);
+		}
+		return res;
+	}
+	
+	private String shiftBacktoStart(String s){
+		char[] arr = s.toCharArray();
+		if(arr.length > 0){
+			int offset = arr[0] - 'a';
+			for(int i = 0; i < arr.length; i++){
+				if(arr[i] - offset < 'a'){
+					arr[i] = (char)(arr[i] - offset + 26);
+				} else {
+					arr[i] = (char)(arr[i] - offset);
+				}
+			}
+		}
+		return new String(arr);
 	}
 }
