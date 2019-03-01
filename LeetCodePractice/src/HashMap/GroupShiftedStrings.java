611b42f60a2dfd46430e9dd3bcec382016e1e649
tenh <tenh@TengdaHe-1.redmond.corp.microsoft.com>
2019-02-28 20:19:46 -0800

    add 2/28 +

---
+++ LeetCodePractice/src/HashMap/GroupShiftedStrings.java
@@ -0,0 +1,17 @@
+package HashMap;
+
+import java.util.*;
+
+/*Given a string, we can "shift" each of its letter to its successive letter, for example: "abc" -> "bcd". We can keep "shifting" which forms the sequence: "abc" -> "bcd" -> ... -> "xyz".
+Given a list of strings which contains only lowercase alphabets, group all strings that belong to the same shifting sequence, return:
+[
+  ["abc","bcd","xyz"],
+  ["az","ba"],
+  ["acef"],
+  ["a","z"]
+]*/
+public class GroupShiftedStrings {
+	public List<List<String>> groupStrings(String[] strings) {
+		
+	}
+}
