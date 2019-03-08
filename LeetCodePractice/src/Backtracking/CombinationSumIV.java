22e69796b007e9699aee5955055aa7d8c2aa98c5
tenh <tenh@TengdaHe-1.redmond.corp.microsoft.com>
2019-03-07 22:20:49 -0800

    add 3/7 more

--- LeetCodePractice/src/Backtracking/CombinationSumIV.java
+++ LeetCodePractice/src/Backtracking/CombinationSumIV.java
@@ -1,4 +1,5 @@
 package Backtracking;
+
 /*Given an integer array with all positive numbers and no duplicates, find the number of possible combinations that add up to a positive integer target.
 
 Example:
@@ -26,6 +27,16 @@ How does it change the problem?
 What limitation we need to add to the question to allow negative numbers?*/
 public class CombinationSumIV {
 	public int combinationSum4(int[] nums, int target) {
-        
-    }
+		int len = nums.length;
+		int[] dp = new int[target + 1];
+		dp[0] = 1;
+		for (int i = 1; i <= target; i++) {
+			for (int j = 0; j < len; j++) {
+				if (i >= nums[j]) {
+					dp[i] += dp[i - nums[j]];
+				}
+			}
+		}
+		return dp[target];
+	}
 }
