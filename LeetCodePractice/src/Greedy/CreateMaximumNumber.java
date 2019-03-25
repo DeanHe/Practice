23d0bd143388f21e85412815dcfc38b95112b76b
tengdahe <tengdahe@192.168.1.5>
2019-03-25 01:36:00 -0700

    add 3/24

--- LeetCodePractice/src/Greedy/CreateMaximumNumber.java
+++ LeetCodePractice/src/Greedy/CreateMaximumNumber.java
@@ -30,6 +30,60 @@ Output:
 [9, 8, 9]*/
 public class CreateMaximumNumber {
 	public int[] maxNumber(int[] nums1, int[] nums2, int k) {
-
+		int len1 = nums1.length;
+		int len2 = nums2.length;
+		int[] res = new int[k];
+		for(int i = Math.max(0,  k - len2); i <= k && i <= len1; i++){
+			int[] candidate = merge(maxArray(nums1, i), maxArray(nums2, k - i), k);
+			if(greater(candidate, 0, res, 0)){
+				res = candidate;
+			}
+		}
+		return res;
+	}
+	
+	private int[] merge(int[] nums1, int[] nums2, int k){
+		int[] res = new int[k];
+		for(int i = 0, j = 0, index = 0; index < k; index++){
+			if(greater(nums1, i, nums2, j)){
+				res[index] = nums1[i];
+				i++;
+			} else {
+				res[index] = nums2[j];
+				j++;
+			}
+		}
+		return res;
+	}
+	
+	private boolean greater(int[] nums1, int i, int[] nums2, int j){
+		int len1 = nums1.length;
+		int len2 = nums2.length;
+		while(i < len1 && j < len2 && nums1[i] == nums2[j]){
+			i++;
+			j++;
+		}
+		if(j == len2){
+			return true;
+		}
+		if(i < len1 && nums1[i] > nums2[j]){
+			return true;
+		}
+		return false;
+	}
+	
+	private int[] maxArray(int[] nums, int k){
+		int len = nums.length;
+		int[] res = new int[k];
+		for(int i = 0, index = 0; i < len; i++){
+			while(index > 0 && len - i + index > k && res[index - 1] < nums[i]){
+				index--;
+			}
+			if(index < k){
+				res[index] = nums[i];
+				index++;
+			}
+		}
+		return res;
 	}
 }
