4e11ff1f7bba7d466c86015b7e6370513d24d062
tengdahe <tengdahe@192.168.1.5>
2019-03-31 10:45:12 -0700

    add 3/31

--- LeetCodePractice/src/DP/RaceCar.java
+++ LeetCodePractice/src/DP/RaceCar.java
@@ -22,6 +22,29 @@ The shortest instruction sequence is "AAARA".
 Your position goes from 0->1->3->7->7->6.*/
 public class RaceCar {
 	public int racecar(int target) {
-        
+        int[] dp = new int[target + 1];
+        for(int i = 1; i <= target; i++){
+        	dp[i] = Integer.MAX_VALUE;
+        	int countOfAbeforeFirstR = 1, j = 1;
+        	while(j < i){
+        		int countOfAbeforeSecondR = 0;
+        		//case 1: 2 R before reaching i
+        		for(int k = 0; k < j;){
+        			dp[i] = Math.min(dp[i], 1 + countOfAbeforeFirstR + 1 + countOfAbeforeSecondR + dp[i - (j - k)]);
+        			k = k + (1 << countOfAbeforeSecondR);
+        			countOfAbeforeSecondR++;
+        		}
+        		j = j + (1 << countOfAbeforeFirstR);
+        		countOfAbeforeFirstR++;
+        	}
+        	if(j == i){
+        		//case 2: reach i without R
+        		dp[i] = Math.min(dp[i], countOfAbeforeFirstR); 
+        	} else {
+        		// case 3: pass i, then R, then reach i
+        		dp[i] = Math.min(dp[i], countOfAbeforeFirstR + 1 + dp[j - i]);
+        	}
+        }
+        return dp[target];
     }
 }
--- LeetCodePractice/src/DP/TwoKeysKeyboard.java
+++ LeetCodePractice/src/DP/TwoKeysKeyboard.java
@@ -23,6 +23,18 @@ Note:
 The n will be in the range [1, 1000].*/
 public class TwoKeysKeyboard {
 	public int minSteps(int n) {
-        
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 0;
+        for(int i = 2; i <= n; i++){
+        	dp[i] = i;
+        	for(int j = i - 1; j >= 2; j--){
+        		if(i % j == 0){
+        			dp[i] = dp[j] + i / j;
+        			break;
+        		}
+        	}
+        }
+        return dp[n];
     }
 }
