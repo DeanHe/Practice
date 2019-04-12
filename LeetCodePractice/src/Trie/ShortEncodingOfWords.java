71b04bffc04dbf474d80bf9624190b724ee2cef8
tengdahe <tengdahe@192.168.1.5>
2019-04-12 10:13:03 -0700

    add 4/12

--- LeetCodePractice/src/Trie/ShortEncodingOfWords.java
+++ LeetCodePractice/src/Trie/ShortEncodingOfWords.java
@@ -19,15 +19,15 @@ Explanation: S = "time#bell#" and indexes = [0, 2, 5].*/
 public class ShortEncodingOfWords {
 	class TrieNode {
 		TrieNode[] children;
-		int count;
+		int childrenCount;
 		TrieNode() {
 			children = new TrieNode[26];
-			count = 0;
+			childrenCount = 0;
 		}
 		TrieNode get(char c){
 			if(children[c- 'a'] == null){
 				children[c- 'a'] = new TrieNode();
-				count++;
+				childrenCount++;
 			}
 			return children[c- 'a'];
 		}
@@ -37,11 +37,11 @@ public class ShortEncodingOfWords {
 	//After, the leaves of this trie (nodes with no children) represent words that have no suffix, 
 	//and we will count sum(word.length + 1 for word in words).
 	public int minimumLengthEncoding(String[] words) {
-        TrieNode trie = new TrieNode();
+        TrieNode root = new TrieNode();
         Map<TrieNode, Integer> NodeWordIndex = new HashMap<>();
         for(int i = 0; i < words.length; i++){
         	String word = words[i];
-        	TrieNode cur = trie;
+        	TrieNode cur = root;
         	for(int j = word.length() - 1; j >= 0; j--){
         		char c = word.charAt(j);
         		cur = cur.get(c);
@@ -50,7 +50,7 @@ public class ShortEncodingOfWords {
         }
         int ans = 0;
         for(TrieNode cur : NodeWordIndex.keySet()){
-        	if(cur.count == 0){
+        	if(cur.childrenCount == 0){
         		String word = words[NodeWordIndex.get(cur)];
         		ans += word.length() + 1;
         	}
