884a702c806f835df4f67e1110ed0168beb931c4
tenh <tenh@TengdaHe-1.redmond.corp.microsoft.com>
2019-04-14 11:44:39 -0700

    add 4/14

--- LeetCodePractice/src/BinarySearch/SearchInRotatedSortedArray.java
+++ LeetCodePractice/src/BinarySearch/SearchInRotatedSortedArray.java
@@ -5,41 +5,34 @@ public class SearchInRotatedSortedArray {
 		if (A == null || A.length == 0) {
 			return -1;
 		}
-		int low = 0;
-		int high = A.length - 1;
-		while (low <= high) {
-			int mid = (low + high) / 2;
+		int start = 0;
+		int end = A.length - 1;
+		while (start + 1 < end) {
+			int mid = (start + end) / 2;
 			if (A[mid] == target) {
 				return mid;
-			} else if (target < A[mid]) {
-				// right side in order
-				if (A[mid] < A[high]) {
-					high = mid - 1;
+			} else if (A[start] < A[mid]) {
+				// left side from beginning in order
+				if(A[start] <= target && target <= A[mid]){
+					end = mid;
+				} else {
+					start = mid;
 				}
-				// left side in order
+			} else {
+				// right side to end in order
+				if (A[mid] <= target && target <= A[end]) {
+					start = mid;
+				}
 				else {
-					if (target >= A[low]) {
-						high = mid - 1;
-					} else {
-						low = mid + 1;
-					}
+					end = mid;
 				}
 			}
-			// target > A[mid]
-			else {
-				// left side in order
-				if (A[low] < A[mid]) {
-					low = mid + 1;
-				}
-				// right side in order
-				else {
-					if (target <= A[high]) {
-						low = mid + 1;
-					} else {
-						high = mid - 1;
-					}
-				}
-			}
+		}
+		if(A[start] == target){
+			return start;
+		}
+		if(A[end] == target){
+			return end;
 		}
 		return -1;
 	}
