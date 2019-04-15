51d2af2961c14c440b3525a0355ccc4e70d3e42c
tengdahe <tengdahe@192.168.1.5>
2019-04-15 10:27:01 -0700

    add 4/15

---
+++ LeetCodePractice/src/Backtracking/WordPatternII.java
@@ -0,0 +1,22 @@
+package Backtracking;
+/*Given a pattern and a string str, find if str follows the same pattern.
+
+Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty substring in str.(i.e if a corresponds to s, then b cannot correspond to s. For example, given pattern = "ab", str = "ss", return false.)
+
+Example
+Given pattern = "abab", str = "redblueredblue", return true.
+Given pattern = "aaaa", str = "asdasdasdasd", return true.
+Given pattern = "aabb", str = "xyzabcxzyabc", return false.
+
+Notice
+You may assume both pattern and str contains only lowercase letters.*/
+public class WordPatternII {
+	/**
+     * @param pattern: a string,denote pattern string
+     * @param str: a string, denote matching string
+     * @return: a boolean
+     */
+    public boolean wordPatternMatch(String pattern, String str) {
+        // write your code here
+    }
+}
--- LeetCodePractice/src/String/RepeatedSubstringPattern.java
+++ LeetCodePractice/src/String/RepeatedSubstringPattern.java
@@ -1,5 +1,21 @@
 package String;
+/*Given a non-empty string check if it can be constructed by taking a substring of it and appending multiple copies of the substring together. You may assume the given string consists of lowercase English letters only and its length will not exceed 10000.
 
+Example 1:
+
+Input: "abab"
+Output: True
+Explanation: It's the substring "ab" twice.
+Example 2:
+
+Input: "aba"
+Output: False
+Example 3:
+
+Input: "abcabcabcabc"
+Output: True
+Explanation: It's the substring "abc" four times. (And the substring "abcabc" twice.)
+*/
 public class RepeatedSubstringPattern {
 	public boolean repeatedSubstringPattern(String s) {
         for(int len = s.length() / 2; len >= 1; len--){
