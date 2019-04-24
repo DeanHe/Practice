26fd18ee95ce02012f681e18acfde6001a922390
tengdahe <tengdahe@192.168.1.5>
2019-04-24 09:32:54 -0700

    add 4/24 9:32

--- LeetCodePractice/src/DP/KInversePairsArray.java
+++ LeetCodePractice/src/DP/KInversePairsArray.java
@@ -1,4 +1,5 @@
 package DP;
+
 /*Given two integers n and k, find how many different arrays consist of numbers from 1 to n such that there are exactly k inverse pairs.
 
 We define an inverse pair as following: For ith and jth element in the array, if i < j and a[i] > a[j] then it's an inverse pair; Otherwise, it's not.
@@ -24,12 +25,31 @@ The array [1,3,2] and [2,1,3] have exactly 1 inverse pair.
 Note:
 
 The integer n is in the range [1, 1000] and k is in the range [0, 1000].*/
+//https://leetcode.com/problems/k-inverse-pairs-array/discuss/104822/Python-Straightforward-with-Explanation
+//https://leetcode.com/problems/k-inverse-pairs-array/discuss/104815/Java-DP-O(nk)-solution
 public class KInversePairsArray {
-	
+
 	public int kInversePairs(int n, int k) {
 		int mod = 1000000007;
-		if(k > n*(n - 1) / 2 || k < 0){
+		if (k > n * (n - 1) / 2 || k < 0) {
 			return 0;
 		}
-    }
+		if (k == 0 || k == n * (n - 1) / 2) {
+			return 1;
+		}
+		long[][] dp = new long[n + 1][k + 1];
+		dp[2][0] = 1;
+		dp[2][1] = 1;
+		for (int i = 3; i <= n; i++) {
+			dp[i][0] = 1;
+			for (int j = 1; j <= Math.min(k, i * (i - 1) / 2); j++) {
+				dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+				if (j >= i) {
+					dp[i][j] -= dp[i - 1][j - i];
+				}
+				dp[i][j] = (dp[i][j] + mod) % mod;
+			}
+		}
+		return (int) dp[n][k];
+	}
 }
