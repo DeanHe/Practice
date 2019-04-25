4f7005a8ef1f95ef72c8d804759f165d26e1565f
tengdahe <tengdahe@192.168.1.5>
2019-04-25 09:46:23 -0700

    add 4/25 9:46

--- LeetCodePractice/src/DP/PerfectSquares.java
+++ LeetCodePractice/src/DP/PerfectSquares.java
@@ -1,6 +1,8 @@
 package DP;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /*Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
 
@@ -28,4 +30,30 @@ public class PerfectSquares {
        }
        return dp[n];
     }
+	
+	public int numSquaresBFS(int n) {
+	       boolean[] visited = new boolean[n + 1];
+	       visited[n] = true;
+	       Queue<Integer> queue = new LinkedList<>();
+	       queue.offer(n);
+	       int steps = 0;
+	       while(!queue.isEmpty()){
+	    	   int len = queue.size();
+	    	   for(int i = 0; i < len; i++){
+	    		   int cur = queue.poll();
+	    		   if(cur == 0){
+	    			   return steps;
+	    		   }
+	    		   for(int j = 1; j * j <= cur; j++){
+	    			   int num = cur - j * j;
+	    			   if(!visited[num]){
+	    				   queue.offer(num);
+	    				   visited[num] = true;
+	    			   }
+	    		   }
+	    	   }
+	    	   steps++;
+	       }
+	       return -1;       
+	    }
 }
--- LeetCodePractice/src/Solution.java
+++ LeetCodePractice/src/Solution.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.PriorityQueue;
+import java.util.Stack;
 
 import SweepLine.Interval;
 
@@ -13,62 +14,4 @@ public class Solution {
 		System.out.println(reStrings);
 	}
 	
-	public boolean canPlaceFlowers(int[] flowerBed, int n){
-		int count = 0;
-		for(int i = 0; i < flowerBed.length; i++){
-			if(flowerBed[i] == 0 
-					&& (i == 0 || flowerBed[i - 1] == 0) 
-					&& (i == flowerBed.length - 1 || flowerBed[i + 1] == 0)){
-				flowerBed[i] = 1;
-				count++;
-				if(count >= n){
-					return true;
-				}
-			}
-		}
-		return false;
-	}
-	
-	public static List<String> subStringsKDist(String inputStr, int num)
-	{
-        // WRITE YOUR CODE HERE
-        List<String> res = new ArrayList<>();
-        int len = inputStr.length();
-        int[] count = new int[26];
-        int distinctCount;
-        for(int i = 0; i < len; i++){
-            Arrays.fill(count, 0);
-            distinctCount = 0;
-            for(int j = i; j < len; j++){
-                if(count[inputStr.charAt(j) - 'a'] == 0){
-                    distinctCount++;
-                }
-                count[inputStr.charAt(j) - 'a']++;
-                if(distinctCount == num){
-                    String temp = inputStr.substring(i, j + 1);
-                    res.add(temp);
-                }
-            }
-        }
-        return res;
-    }
-	
-	public ArrayList<Interval> insert(ArrayList<Interval> intervals, Interval newInterval) {
-        ArrayList<Interval> result = new ArrayList<Interval>();
-        int insertPos = 0;
-        for(Interval i : intervals){
-            if(i.end < newInterval.start){
-            	result.add(i);
-            	insertPos++;
-            } else if(i.start > newInterval.end){
-            	result.add(i);
-            } else {
-            	newInterval.start = Math.min(newInterval.start, i.start);
-            	newInterval.end = Math.max(newInterval.end, i.end);
-            }
-        }
-        result.add(insertPos, newInterval);
-        // write your code here
-        return result;
-    }
 }
