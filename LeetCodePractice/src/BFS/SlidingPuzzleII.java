eb686c798ffd8d20a836c954d92ecc2d428ae165
tengdahe <tengdahe@192.168.1.5>
2019-05-06 10:14:57 -0700

    add 5/6 10:14

--- LeetCodePractice/src/BFS/SlidingPuzzleII.java
+++ LeetCodePractice/src/BFS/SlidingPuzzleII.java
@@ -55,13 +55,87 @@ Output:
 Challenge
 How to optimize the memory?
 Can you solve it with A* algorithm?*/
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+
 public class SlidingPuzzleII {
 	/**
-     * @param init_state: the initial state of chessboard
-     * @param final_state: the final state of chessboard
-     * @return: return an integer, denote the number of minimum moving
-     */
-    public int minMoveStep(int[][] init_state, int[][] final_state) {
-        // # write your code here
-    }
+	 * @param init_state:
+	 *            the initial state of chessboard
+	 * @param final_state:
+	 *            the final state of chessboard
+	 * @return: return an integer, denote the number of minimum moving
+	 */
+	int rows, cols;
+	int[] dirs = { 0, 1, 0, -1, 0 };
+
+	public int minMoveStep(int[][] init_state, int[][] final_state) {
+		rows = init_state.length;
+		cols = init_state[0].length;
+		String start = matrixToString(init_state);
+		String target = matrixToString(final_state);
+		Set<String> visited = new HashSet<>();
+		Queue<String> queue = new LinkedList<>();
+		queue.offer(start);
+		visited.add(start);
+		int move = 0;
+		while (!queue.isEmpty()) {
+			int size = queue.size();
+			for (int i = 0; i < size; i++) {
+				String cur = queue.poll();
+				if (cur.equals(target)) {
+					return move;
+				}
+				List<String> nbs = getNeighbors(cur);
+				for (String nb : nbs) {
+					if (!visited.contains(nb)) {
+						queue.offer(nb);
+						visited.add(nb);
+					}
+				}
+			}
+			move++;
+		}
+		return -1;
+	}
+
+	private String matrixToString(int[][] matrix) {
+		StringBuilder sb = new StringBuilder();
+		for (int r = 0; r < rows; r++) {
+			for (int c = 0; c < cols; c++) {
+				sb.append(matrix[r][c]);
+			}
+		}
+		return sb.toString();
+	}
+
+	private List<String> getNeighbors(String s) {
+		List<String> res = new ArrayList<>();
+		int zeroIdx = s.indexOf("0");
+		int zeroRow = zeroIdx / cols;
+		int zeroCol = zeroIdx % cols;
+		for (int i = 0; i < dirs.length - 1; i++) {
+			int nbRow = zeroRow + dirs[i];
+			int nbCol = zeroCol + dirs[i + 1];
+			if (nbRow < 0 || nbRow >= rows || nbCol < 0 || nbCol >= cols) {
+				continue;
+			}
+			int nbIdx = nbRow * cols + nbCol;
+			res.add(swap(s, zeroIdx, nbIdx));
+		}
+		return res;
+	}
+
+	private String swap(String s, int i, int j) {
+		char[] arr = s.toCharArray();
+		char temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+		return String.valueOf(arr);
+	}
 }
