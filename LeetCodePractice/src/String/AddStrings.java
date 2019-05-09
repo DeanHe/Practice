914a2332a1a05c75a283e90be929cf522f590fc2
tenh <tenh@TengdaHe-1.redmond.corp.microsoft.com>
2019-05-08 20:20:24 -0700

    add 5/8 8:20

--- LeetCodePractice/src/String/AddStrings.java
+++ LeetCodePractice/src/String/AddStrings.java
@@ -11,6 +11,22 @@ similar: AddTwoNumber*/
 
 public class AddStrings {
 	public String addStrings(String num1, String num2) {
-        
+        int len1 = num1.length(), len2 = num2.length();
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        for(int i = len1 -1, j = len2 - 1; i >= 0 || j >= 0; i--, j--){
+        	if(i >= 0){
+        		carry += num1.charAt(i) - '0';
+        	}
+        	if(j >= 0){
+        		carry += num2.charAt(j) - '0';
+        	}
+        	sb.append(carry % 10);
+        	carry /= 10;
+        }
+        if(carry > 0){
+        	sb.append(carry);
+        }
+        return sb.reverse().toString();
     }
 }
