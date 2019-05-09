81e73de3b4584e8bc389a3f43fc8a2dc86fcdf10
tengdahe <tengdahe@192.168.1.5>
2019-05-09 10:33:20 -0700

    add 5/9 10:32

--- LeetCodePractice/src/Stack/MiniParser.java
+++ LeetCodePractice/src/Stack/MiniParser.java
@@ -23,34 +23,72 @@ Return a NestedInteger object containing a nested list with 2 elements:
     ii. A nested list with one element:
          a. An integer containing value 789.*/
 
-/**
- * // This is the interface that allows for creating nested lists.
- * // You should not implement it, or speculate about its implementation
- * public interface NestedInteger {
- *     // Constructor initializes an empty nested list.
- *     public NestedInteger();
- *
- *     // Constructor initializes a single integer.
- *     public NestedInteger(int value);
- *
- *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
- *     public boolean isInteger();
- *
- *     // @return the single integer that this NestedInteger holds, if it holds a single integer
- *     // Return null if this NestedInteger holds a nested list
- *     public Integer getInteger();
- *
- *     // Set this NestedInteger to hold a single integer.
- *     public void setInteger(int value);
- *
- *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
- *     public void add(NestedInteger ni);
- *
- *     // @return the nested list that this NestedInteger holds, if it holds a nested list
- *     // Return null if this NestedInteger holds a single integer
- *     public List<NestedInteger> getList();
- * }
- */
-public class MiniParser {
+import java.util.*;
 
+public class MiniParser {
+	public NestedInteger deserialize(String s) {
+        if(s == null || s.length() == 0){
+        	return null;
+        }
+        if(s.charAt(0) != '['){
+        	return new NestedInteger(Integer.valueOf(s.charAt(0)));
+        }
+        int len = s.length();
+        int l = 0; // l shall point to the start of a number substring;
+        		   // r shall point to the end+1 of a number substring;
+        NestedInteger cur = null;
+        for(int i = 0; i < len; i++){
+        	char c = s.charAt(i);
+        	if(c == '['){
+        		
+        	} else if(c == ']'){
+        		
+        	} else if(c ==',') {
+        		
+        	}
+        }
+        return cur;
+    }
+	
+
+	  // This is the interface that allows for creating nested lists.
+	  // You should not implement it, or speculate about its implementation
+	  class NestedInteger {
+	      // Constructor initializes an empty nested list.
+	      public NestedInteger(){
+	    	  
+	      };
+	 
+	      // Constructor initializes a single integer.
+	      public NestedInteger(int value){
+	    	  
+	      };
+	 
+	      // @return true if this NestedInteger holds a single integer, rather than a nested list.
+	      public boolean isInteger(){
+	    	  return false;
+	      };
+	 
+	      // @return the single integer that this NestedInteger holds, if it holds a single integer
+	      // Return null if this NestedInteger holds a nested list
+	      public Integer getInteger(){;
+	      	return 0;
+	      }
+	 
+	      // Set this NestedInteger to hold a single integer.
+	      public void setInteger(int value){
+	    	  
+	      };
+	 
+	      // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+	      public void add(NestedInteger ni){
+	    	  
+	      };
+	 
+	      // @return the nested list that this NestedInteger holds, if it holds a nested list
+	      // Return null if this NestedInteger holds a single integer
+	      public List<NestedInteger> getList(){
+	      	return new ArrayList<NestedInteger>();
+	      }
+	  }
 }
