6a06a93537c9e6a77184597766c0fa0e18a5ec59
tenh <tenh@TengdaHe-1.redmond.corp.microsoft.com>
2019-05-20 19:52:31 -0700

    add 5/20 7:52

---
+++ LeetCodePractice/src/DP/FourKeysKeyboard.java
@@ -0,0 +1,34 @@
+package DP;
+
+/*Imagine you have a special keyboard with the following keys:
+
+Key 1: (A): Print one 'A' on screen.
+Key 2: (Ctrl-A): Select the whole screen.
+Key 3: (Ctrl-C): Copy selection to buffer.
+Key 4: (Ctrl-V): Print buffer on screen appending it after what has already been printed.
+Now, you can only press the keyboard for N times (with the above four keys), find out the maximum numbers of 'A' you can print on screen.
+
+Example
+Example 1:
+
+Input: 3
+Output: 3
+Explanation: A, A, A
+Example 2:
+
+Input: 7
+Output: 9
+Explanation: A, A, A, Ctrl A, Ctrl C, Ctrl V, Ctrl V
+Notice
+1 <= N <= 50
+Answers will be in the range of 32-bit signed integer.*/
+
+public class FourKeysKeyboard {
+	/**
+     * @param N: an integer
+     * @return: return an integer
+     */
+    public int maxA(int N) {
+        // write your code here
+    }
+}
