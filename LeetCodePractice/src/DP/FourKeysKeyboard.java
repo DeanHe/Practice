07ccd12754c33fdccdcda5fa3f1ef8ae5dc6be11
tengdahe <tengdahe@192.168.1.101>
2019-05-22 10:25:07 -0700

    add 5/22 10:25

--- LeetCodePractice/src/DP/FourKeysKeyboard.java
+++ LeetCodePractice/src/DP/FourKeysKeyboard.java
@@ -21,7 +21,12 @@ Output: 9
 Explanation: A, A, A, Ctrl A, Ctrl C, Ctrl V, Ctrl V
 Notice
 1 <= N <= 50
-Answers will be in the range of 32-bit signed integer.*/
+Answers will be in the range of 32-bit signed integer.
+
+the optimal sequence A count dp[i] comes from:
+1 continuous A
+2 .... Ctrl-A, Ctrl-C, multiple Ctrl-V
+*/
 
 public class FourKeysKeyboard {
 	/**
@@ -30,5 +35,14 @@ public class FourKeysKeyboard {
      */
     public int maxA(int N) {
         // write your code here
+    	int[] dp = new int[N];
+    	dp[0] = 1;
+    	for(int i = 1; i < N; i++){
+    		dp[i] = dp[i - 1] + 1;
+    		for(int j = 0; j <= i - 3; j++){
+    			dp[i] = Math.max(dp[i], dp[j] * (i - j - 1));
+    		}
+    	}
+    	return dp[N - 1];
     }
 }
