8573ee6762e4da3fede5fd01f96fe32ab0088e49
tengdahe <tengdahe@192.168.1.101>
2019-06-10 09:52:29 -0700

    NumberOfSubmatricesThatSumToTarget

--- LeetCodePractice/src/PrefixSum/NumberOfSubmatricesThatSumToTarget.java
+++ LeetCodePractice/src/PrefixSum/NumberOfSubmatricesThatSumToTarget.java
@@ -1,4 +1,8 @@
 package PrefixSum;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /*Given a matrix, and a target, return the number of non-empty submatrices that sum to target.
 
 A submatrix x1, y1, x2, y2 is the set of all cells matrix[x][y] with x1 <= x <= x2 and y1 <= y <= y2.
@@ -29,5 +33,33 @@ public class NumberOfSubmatricesThatSumToTarget {
 	public int numSubmatrixSumTarget(int[][] matrix, int target) {
         int rows = matrix.length;
         int cols = matrix[0].length;
+        int[][] preSum = new int[rows][cols];
+        for(int r = 0; r < rows; r++){
+        	for(int c = 0; c < cols; c++){
+        		if(c == 0){
+        			preSum[r][c] = matrix[r][c];
+        		} else {
+        			preSum[r][c] = preSum[r][c - 1] + matrix[r][c];
+        		}
+        	}
+        }
+        int res = 0;
+        for(int c = 0; c < cols; c++){
+        	for(int i = c; i < cols; i++){
+        		Map<Integer, Integer> counter = new HashMap<>();
+        		counter.put(0, 1);
+        		int cur = 0;
+        		for(int r = 0; r < rows; r++){
+        			if(c == 0){
+            			cur += preSum[r][i];
+            		} else {
+            			cur += preSum[r][i] - preSum[r][c - 1];
+            		}
+        			res += counter.getOrDefault(cur - target, 0);
+        			counter.put(cur, counter.getOrDefault(cur, 0) + 1);
+        		}
+        	}
+        }
+        return res;                         
     }
 }
