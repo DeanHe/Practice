d6d9f043a084e784dcaa11090da5aadcb0a528a5
tengdahe <tengdahe@192.168.1.101>
2019-06-22 13:16:53 -0700

    ReverseWordsInaStringII;MinimumWindowSubstring

--- LeetCodePractice/src/BFS/CourseSchedule.java
+++ LeetCodePractice/src/BFS/CourseSchedule.java
@@ -16,79 +16,82 @@ Return false*/
 
 public class CourseSchedule {
 	/*
-     * @param numCourses: a total of n courses
-     * @param prerequisites: a list of prerequisite pairs
-     * @return: true if can finish all courses or false
-     */
-	//BFS
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        // write your code here
-    	List<List<Integer>> graph = new ArrayList<>(numCourses);
-    	   for(int i = 0; i < numCourses; i++){
-    	       graph.add(new ArrayList<>());
-    	   }
-    	   int[] indegree = new int[numCourses];
-    	    
-    	   for(int i =0; i < prerequisites.length; i++){
-    	       int post = prerequisites[i][0];
-    	       int pre = prerequisites[i][1];
-    	       graph.get(pre).add(post);
-    	      indegree[post]++;
-    	   }
-    	    
-    	   int count = 0;
-    	   Queue<Integer> queue = new LinkedList<Integer>();
-    	   for(int i = 0; i < indegree.length; i++){
-    	       if(indegree[i]==0){
-    	           //vertex without pre
-    	           queue.offer(i);
-    	       }
-    	   }
-    	   while(!queue.isEmpty()){
-    	       int course = queue.poll();
-    	       count++;
-    	       int size = graph.get(course).size();
-    	       for(int i = 0; i < size; i++){
-    	               if(--indegree[i] == 0){
-    	                   queue.offer(i);
-    	               }
-    	       }
-    	   }
-    	   return count == numCourses;
-    }
-    
-    //DFS
-    public boolean canFinishDFS(int numCourses, int[][] prerequisites) {
-       List<List<Integer>> graph = new ArrayList<>(numCourses);
-       boolean[] visited = new boolean[numCourses];
-       for(int i = 0; i < numCourses; i++){
-           graph.add(new ArrayList<Integer>());
-       }
-       for(int i = 0; i < prerequisites.length; i++){
-            int pre = prerequisites[i][1];
-            int post = prerequisites[i][0];
-           graph.get(pre).add(post);
-       }
-       for(int i = 0; i < numCourses; i++){
-           if(!dfs(graph,visited,i)){
-               return false;
-           }
-       }
-       return true;
-    }
-     
-    private boolean dfs(List<List<Integer>> graph, boolean[] visited, int cur){
-        //true- no cycle; fasle - with cycle
-        if(visited[cur]){
-            return false;
-        }
-        visited[cur] = true; 
-        for(int i = 0; i < graph.get(cur).size(); i++){
-            if(!dfs(graph, visited, (int)graph.get(cur).get(i))){
-                return false;
-            }
-        }
-        visited[cur] = false;
-        return true;
-    }
+	 * @param numCourses: a total of n courses
+	 * 
+	 * @param prerequisites: a list of prerequisite pairs
+	 * 
+	 * @return: true if can finish all courses or false
+	 */
+	// BFS
+	public boolean canFinish(int numCourses, int[][] prerequisites) {
+		// write your code here
+		List<List<Integer>> graph = new ArrayList<>(numCourses);
+		for (int i = 0; i < numCourses; i++) {
+			graph.add(new ArrayList<>());
+		}
+		int[] indegree = new int[numCourses];
+
+		for (int i = 0; i < prerequisites.length; i++) {
+			int post = prerequisites[i][0];
+			int pre = prerequisites[i][1];
+			graph.get(pre).add(post);
+			indegree[post]++;
+		}
+
+		int count = 0;
+		Queue<Integer> queue = new LinkedList<Integer>();
+		for (int i = 0; i < indegree.length; i++) {
+			if (indegree[i] == 0) {
+				// vertex without pre
+				queue.offer(i);
+			}
+		}
+		while (!queue.isEmpty()) {
+			int course = queue.poll();
+			count++;
+			List<Integer> nbs = graph.get(course);
+			for (int nb : nbs) {
+				if (--indegree[nb] == 0) {
+					queue.offer(nb);
+				}
+			}
+		}
+		return count == numCourses;
+	}
+
+	// DFS
+	public boolean canFinishDFS(int numCourses, int[][] prerequisites) {
+		List<List<Integer>> graph = new ArrayList<>(numCourses);
+		boolean[] visited = new boolean[numCourses];
+		for (int i = 0; i < numCourses; i++) {
+			graph.add(new ArrayList<Integer>());
+		}
+		for (int i = 0; i < prerequisites.length; i++) {
+			int pre = prerequisites[i][1];
+			int post = prerequisites[i][0];
+			graph.get(pre).add(post);
+		}
+		for (int i = 0; i < numCourses; i++) {
+			if (!dfs(graph, visited, i)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private boolean dfs(List<List<Integer>> graph, boolean[] visited, int cur) {
+		// true- no cycle; fasle - with cycle
+		if (visited[cur]) {
+			return false;
+		}
+		visited[cur] = true;
+		List<Integer> nbs = graph.get(cur);
+		for (int nb : nbs) {
+			if (!dfs(graph, visited, nb)) {
+				return false;
+			}
+		}
+		visited[cur] = false;
+		return true;
+	}
 }
--- LeetCodePractice/src/BFS/CourseScheduleII.java
+++ LeetCodePractice/src/BFS/CourseScheduleII.java
@@ -15,47 +15,96 @@ Return [0,1]
 
 Given n = 4, prerequisites = [1,0],[2,0],[3,1],[3,2]]
 Return [0,1,2,3] or [0,2,1,3]*/
-		
+
 public class CourseScheduleII {
 	/**
-     * @param numCourses a total of n courses
-     * @param prerequisites a list of prerequisite pairs
-     * @return the course order
-     */
-    public int[] findOrder(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> edges = new ArrayList<List<Integer>>(numCourses);
-        for(int i = 0; i < numCourses; i++){
-            edges.add(new ArrayList<Integer>());
-        }
-        int[] indegree = new int[numCourses];
-        for(int i = 0; i < prerequisites.length; i++){
-            int pre = prerequisites[i][1];
-            int post = prerequisites[i][0];
-            edges.get(pre).add(post);
-            indegree[post]++;
-        }
-        Queue<Integer> queue = new LinkedList<>();
-        for(int i = 0; i < indegree.length; i++){
-            if(indegree[i] == 0){
-                queue.offer(i);
-            }
-        }
-        int count = 0;
-        int[] res = new int[numCourses];
-        while(!queue.isEmpty()){
-            int course = queue.poll();
-            res[count] = course;
-            count++;
-            for(Integer i : edges.get(course)){
-                if(--indegree[i] == 0){
-                    queue.offer(i);
-                }
-            }
-        }
-        if(count == numCourses){
-            return res;
-        } else {
-            return new int[0];
-        }
-    }
+	 * @param numCourses
+	 *            a total of n courses
+	 * @param prerequisites
+	 *            a list of prerequisite pairs
+	 * @return the course order
+	 */
+	public int[] findOrder(int numCourses, int[][] prerequisites) {
+		List<List<Integer>> graph = new ArrayList<List<Integer>>(numCourses);
+		for (int i = 0; i < numCourses; i++) {
+			graph.add(new ArrayList<Integer>());
+		}
+		int[] indegree = new int[numCourses];
+		for (int i = 0; i < prerequisites.length; i++) {
+			int pre = prerequisites[i][1];
+			int post = prerequisites[i][0];
+			graph.get(pre).add(post);
+			indegree[post]++;
+		}
+		Queue<Integer> queue = new LinkedList<>();
+		for (int i = 0; i < indegree.length; i++) {
+			if (indegree[i] == 0) {
+				queue.offer(i);
+			}
+		}
+		int count = 0;
+		int[] res = new int[numCourses];
+		while (!queue.isEmpty()) {
+			int course = queue.poll();
+			res[count] = course;
+			count++;
+			List<Integer> nbs = graph.get(course);
+			for (int nb : nbs) {
+				if (--indegree[nb] == 0) {
+					queue.offer(nb);
+				}
+			}
+		}
+		if (count == numCourses) {
+			return res;
+		} else {
+			return new int[0];
+		}
+	}
+
+	public int[] findOrderDFS(int numCourses, int[][] prerequisites) {
+		List<List<Integer>> graph = new ArrayList<List<Integer>>(numCourses);
+		for (int i = 0; i < numCourses; i++) {
+			graph.add(new ArrayList<Integer>());
+		}
+		for (int i = 0; i < prerequisites.length; i++) {
+			int pre = prerequisites[i][1];
+			int post = prerequisites[i][0];
+			graph.get(pre).add(post);
+		}
+		boolean[] visited = new boolean[numCourses];
+		boolean[] verified = new boolean[numCourses];
+		List<Integer> res = new ArrayList<>();
+		for (int i = 0; i < numCourses; i++) {
+			if (!dfs(graph, res, visited, verified, i)) {
+				return new int[0];
+			}
+		}
+		int[] arr = new int[res.size()];
+		for (int i = 0; i < res.size(); i++) {
+			arr[i] = res.get(res.size() - 1 - i);
+		}
+		return arr;
+	}
+
+	private boolean dfs(List<List<Integer>> graph, List<Integer> res, boolean[] visited, boolean[] verified, int cur) {
+		// true- no cycle; fasle - with cycle
+		if (verified[cur]) {
+			return true;
+		}
+		if (visited[cur]) {
+			return false;
+		}
+		visited[cur] = true;
+		List<Integer> nbs = graph.get(cur);
+		for (int nb : nbs) {
+			if (!dfs(graph, res, visited, verified, nb)) {
+				return false;
+			}
+		}
+		res.add(cur);
+		verified[cur] = true;
+		visited[cur] = false;
+		return true;
+	}
 }
