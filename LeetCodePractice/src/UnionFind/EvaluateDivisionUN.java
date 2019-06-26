3b5aa4edb362b5e8f3d4943898d9ad656045c9e3
tengdahe <tengdahe@192.168.1.101>
2019-06-26 10:12:30 -0700

    EvaluateDivisionUN

--- LeetCodePractice/src/UnionFind/EvaluateDivisionUN.java
+++ LeetCodePractice/src/UnionFind/EvaluateDivisionUN.java
@@ -15,59 +15,59 @@ values = [2.0, 3.0],
 queries = [ ["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x", "x"] ]. 
 The input is always valid. You may assume that evaluating the queries will result in no division by zero and there is no contradiction.*/
 
-import java.util.HashMap;
+import java.util.*;
 import java.util.Map;
 
 public class EvaluateDivisionUN {
 	Map<String, String> parent = new HashMap<>();
 	Map<String, Double> dist = new HashMap<>();
-	public double[] calcEquation(String[][] equations, double[] values, String[][] queries) {
+
+	public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
 		int len = values.length;
-		double[] res = new double[len];
-		for(int i = 0; i < len; i++){
-			String startRoot = findRoot(parent, equations[i][0]);
-			String endRoot = findRoot(parent, equations[i][1]);
-			if(!startRoot.equals(endRoot)){
+		double[] res = new double[queries.size()];
+		for (int i = 0; i < len; i++) {
+			String nu = equations.get(i).get(0);
+			String de = equations.get(i).get(1);
+			String startRoot = findRoot(parent, dist, nu);
+			String endRoot = findRoot(parent, dist, de);
+			if (!startRoot.equals(endRoot)) {
 				parent.put(startRoot, endRoot);
-				dist.put(startRoot, dist.get(endRoot) * values[i] / dist.get(startRoot));
+				dist.put(startRoot, dist.get(de) * values[i] / dist.get(nu));
 			}
 		}
-		for(int i = 0; i < queries.length; i++){
-			if(!parent.containsKey(queries[i][0]) || !parent.containsKey(queries[i][1])){
-				continue;
-			}
-			String startRoot = findRoot(parent, queries[i][0]);
-			String endRoot = findRoot(parent, queries[i][1]);
-			if(!startRoot.equals(endRoot)){
+		for (int i = 0; i < queries.size(); i++) {
+			String nu = queries.get(i).get(0);
+			String de = queries.get(i).get(1);
+			if (!parent.containsKey(nu) || !parent.containsKey(de)) {
 				res[i] = -1.0;
 				continue;
 			}
-			res[i] = dist.get(startRoot) / dist.get(endRoot);
+			String startRoot = findRoot(parent, dist, nu);
+			String endRoot = findRoot(parent, dist, de);
+			if (!startRoot.equals(endRoot)) {
+				res[i] = -1.0;
+				continue;
+			}
+			res[i] = dist.get(nu) / dist.get(de);
 		}
+		return res;
 	}
-	private String findRoot(Map<String, String> parent, Map<String, Double> dist, String s){
-		if(!parent.containsKey(s)){
+
+	private String findRoot(Map<String, String> parent, Map<String, Double> dist, String s) {
+		if (!parent.containsKey(s)) {
 			parent.put(s, s);
 			dist.put(s, 1.0);
 			return s;
 		}
-		while(!parent.get(s).equals(s)){
-			String father = parent.get(s);
-			s = father;
+		String root = s;
+		double val = 1.0;
+		while (!parent.get(root).equals(root)) {
+			String father = parent.get(root);
+			val *= dist.get(root);
+			root = father;
 		}
-		
-	}
-	
-	private int findRoot(int x) {
-		int root = x;
-		while (id[root] != root) {
-			root = id[root];
-		}
-		while (id[x] != root) {
-			int father = id[x];
-			id[x] = root;
-			x = father;
-		}
+		parent.put(s, root);
+		dist.put(s, val);
 		return root;
 	}
 }
