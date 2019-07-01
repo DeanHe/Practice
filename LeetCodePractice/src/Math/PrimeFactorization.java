5fb45b180e4b23cd767d89c3af1a8530a1908df2
tengdahe <tengdahe@192.168.1.101>
2019-07-01 10:30:31 -0700

    IntersectionOfTwoArrays;PrimeFactorization

---
+++ LeetCodePractice/src/Math/PrimeFactorization.java
@@ -0,0 +1,37 @@
+package Math;
+
+import java.util.*;
+
+/*Prime factorize a given integer.
+
+Example
+Example 1:
+
+Input: 10
+Output: [2, 5]
+Example 2:
+
+Input: 660
+Output: [2, 2, 3, 5, 11]
+Notice
+You should sort the factors in ascending order.*/
+public class PrimeFactorization {
+	/**
+     * @param num: An integer
+     * @return: an integer array
+     */
+    public List<Integer> primeFactorization(int num) {
+        // write your code here
+        List<Integer> factors = new ArrayList<>();
+        for(int i = 2; i * i <= num; i++){
+            while(num % i == 0){
+                factors.add(i);
+                num /= i;
+            }
+        }
+        if(num != 1){
+            factors.add(num);
+        }
+        return factors;
+    }
+}
