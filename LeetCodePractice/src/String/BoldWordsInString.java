96642036fa78127209e59bae0b599b97c815f8e3
tenh <tenh@TengdaHe-1.redmond.corp.microsoft.com>
2019-07-02 17:25:11 -0700

    MonotonicArray;BoldWordsInString

--- LeetCodePractice/src/String/BoldWordsInString.java
+++ LeetCodePractice/src/String/BoldWordsInString.java
@@ -13,9 +13,29 @@ S has length in range [0, 500].
 All characters in words[i] and S are lowercase letters.*/
 public class BoldWordsInString {
 	public String boldWords(String[] words, String s) {
-		int maxLen = 0;
+		int len = s.length();
+		boolean[] mark = new boolean[len];
 		for(String w : words){
-			maxLen = Math.max(maxLen, w.length());
+			int start = 0;
+			int idx = s.indexOf(w, start);
+			while(idx >= 0){
+				for(int i = idx; i < idx + w.length(); i++){
+					mark[i] =  true;
+				}
+				start++;
+				idx = s.indexOf(w, start);
+			}
 		}
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < len; i++){
+			if(mark[i] && (i == 0 || !mark[i - 1])){
+				sb.append("<b>");
+			}
+			sb.append(s.charAt(i));
+			if(mark[i] && (i == len - 1 || !mark[i + 1])){
+				sb.append("</b>");
+			}
+		}
+		return sb.toString();
 	}
 }
