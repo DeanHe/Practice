882b2268366e381d8d256ed67ab01971d220343e
tenh <tenh@jawilleylaptop.redmond.corp.microsoft.com>
2019-07-24 16:25:04 -0700

    NextGreaterElementII

--- LeetCodePractice/src/Stack/NextGreaterElementI.java
+++ LeetCodePractice/src/Stack/NextGreaterElementI.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
 
-/*You are given two arrays (without duplicates) nums1 and nums2 where nums1’s elements are subset of nums2. Find all the next greater numbers for nums1's elements in the corresponding places of nums2.
+/*You are given two arrays (without duplicates) nums1 and nums2 where nums1's elements are subset of nums2. Find all the next greater numbers for nums1's elements in the corresponding places of nums2.
 The Next Greater Number of a number x in nums1 is the first greater number to its right in nums2. If it does not exist, output -1 for this number.
 
 Example
