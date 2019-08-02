fb5a60d844f4a5b6fa24c14379e4e82492fefc90
tengdahe <tengdahe@192.168.1.101>
2019-08-02 10:11:02 -0700

    RangeModule

--- LeetCodePractice/src/SweepLine/RangeModule.java
+++ LeetCodePractice/src/SweepLine/RangeModule.java
@@ -1,4 +1,10 @@
 package SweepLine;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
 /*A Range Module is a module that tracks ranges of numbers. Your task is to design and implement the following interfaces in an efficient manner.
 
 addRange(int left, int right) Adds the half-open interval [left, right), tracking every real number in that interval. Adding an interval that partially overlaps with currently tracked numbers should add any numbers in the interval [left, right) that are not already tracked.
@@ -29,19 +35,53 @@ public class RangeModule {
 	 * boolean param_2 = obj.queryRange(left,right);
 	 * obj.removeRange(left,right);
 	 */
+	TreeMap<Integer, Integer> map;
 	public RangeModule() {
-        
+        map = new TreeMap<>();
     }
     
     public void addRange(int left, int right) {
-        
+    	// find first and last overlapping interval
+        Integer preStart = map.floorKey(left);
+        Integer posStart = map.floorKey(right);
+        if(preStart == null && posStart == null){
+        	map.put(left, right);
+        } else if(preStart != null && map.get(preStart) >= left){
+        	Integer maxEnd = Math.max(map.get(preStart), right);
+        	maxEnd = Math.max(maxEnd, map.get(posStart));
+        	map.put(preStart, maxEnd);
+        } else {
+        	map.put(left, Math.max(map.get(posStart), right));
+        }
+        // clean up intermediate intervals
+        Map<Integer, Integer> overlap = map.subMap(left, false, right, true);
+        Set<Integer> set = new HashSet<>(overlap.keySet());
+        map.keySet().removeAll(set);
     }
     
     public boolean queryRange(int left, int right) {
-        
+        Integer preStart = map.floorKey(left);
+        if(preStart == null){
+        	return false;
+        }
+        return map.get(preStart) >= right;
     }
     
     public void removeRange(int left, int right) {
-        
+        if(left >= right){
+        	return;
+        }
+        Integer preStart = map.floorKey(left);
+        Integer posStart = map.floorKey(right);
+        if(posStart != null && map.get(posStart) > right){
+        	map.put(right, map.get(posStart));
+        }
+        if(preStart != null && map.get(preStart) > left){
+        	map.put(preStart, left);
+        }
+     // clean up intermediate intervals
+        Map<Integer, Integer> overlap = map.subMap(left, true, right, false);
+        Set<Integer> set = new HashSet<>(overlap.keySet());
+        map.keySet().removeAll(set);
     }
 }
