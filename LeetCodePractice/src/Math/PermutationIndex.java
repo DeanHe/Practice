123fdf39256d17bab78356277a6eb8ddf068a8af
tengdahe <tengdahe@192.168.1.103>
2019-08-21 09:13:38 -0700

    PermutationIndexII

--- LeetCodePractice/src/Math/PermutationIndex.java
+++ LeetCodePractice/src/Math/PermutationIndex.java
@@ -9,13 +9,36 @@ Output:1
 Example 2:
 
 Input:[3,2,1]
-Output:6*/
+Output:6
+
+take [4, 2, 1, 3] for example, its index is 3 * 3! + 1 * 2! + 0 * 1! + 0 * 0! + 1
+*/
 public class PermutationIndex {
 	/**
      * @param A: An array of integers
      * @return: A long integer
      */
     public long permutationIndex(int[] A) {
-        // write your code here
+        int len = A.length;
+        long factor = 1;
+        for(int i = 1; i < len; i++){
+        	factor *= i;
+        }
+        long res = 0, top = len - 1;
+        for(int i = 0; i < len; i++){
+        	int count = 0;
+        	for(int j = i + 1; j < len; j++){
+        		if(A[i] > A[j]){
+        			count++;
+        		}
+        	}
+        	res += factor * count;
+        	if(top != 0){
+        		factor /= top;
+            	top--;
+        	}
+        }
+        res++;
+        return res;
     }
 }
---
+++ LeetCodePractice/src/Math/PermutationIndexII.java
@@ -0,0 +1,41 @@
+package Math;
+
+import java.util.*;
+
+/*Given a permutation which may contain repeated numbers, find its index in all the permutations of these numbers, which are ordered in lexicographical order. The index begins at 1.
+
+Example
+Example 1:
+
+Input :[1,4,2,2]
+Output:3
+Example 2:
+
+Input :[1,6,5,3,1]
+Output:24*/
+public class PermutationIndexII {
+	/**
+     * @param A: An array of integers
+     * @return: A long integer
+     */
+    public long permutationIndexII(int[] A) {
+        // write your code here
+    	long res = 0, factorial = 1, dup = 1;
+    	int len = A.length;
+    	Map<Integer, Integer> freqMap = new HashMap<>();
+    	for(int i = len - 1; i >= 0; i--){
+    		freqMap.put(A[i], freqMap.getOrDefault(A[i], 0) + 1);
+    		dup *= freqMap.get(A[i]);
+    		int count = 0;
+    		for(int j = i + 1; j < len; j++){
+    			if(A[i] > A[j]){
+    				count++;
+    			}
+    		}
+    		res += count * factorial / dup;
+    		factorial *= len - i;
+    	}
+    	res++;
+    	return res;
+    }
+}
