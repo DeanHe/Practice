2f13f45acd016862ffe5c576e5093542e2f003e1
tenh <tenh@TengdaHe-1.redmond.corp.microsoft.com>
2019-08-20 18:06:36 -0700

    PermutationIndex

---
+++ LeetCodePractice/src/Math/PermutationIndex.java
@@ -0,0 +1,21 @@
+package Math;
+/*Given a permutation which contains no repeated number, find its index in all the permutations of these numbers, which are ordered in lexicographical order. The index begins at 1.
+
+Example
+Example 1:
+
+Input:[1,2,4]
+Output:1
+Example 2:
+
+Input:[3,2,1]
+Output:6*/
+public class PermutationIndex {
+	/**
+     * @param A: An array of integers
+     * @return: A long integer
+     */
+    public long permutationIndex(int[] A) {
+        // write your code here
+    }
+}
