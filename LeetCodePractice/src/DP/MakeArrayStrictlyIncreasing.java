9880edc89c4fcef70bf7ed2d89e95b2c06ac88ae
tengdahe <tengdahe@192.168.1.103>
2019-09-20 10:45:30 -0700

    comprehend

--- LeetCodePractice/src/DP/MakeArrayStrictlyIncreasing.java
+++ LeetCodePractice/src/DP/MakeArrayStrictlyIncreasing.java
@@ -35,6 +35,7 @@ Constraints:
 0 <= arr1[i], arr2[i] <= 10^9*/
 public class MakeArrayStrictlyIncreasing {
 	public int makeArrayIncreasing(int[] arr1, int[] arr2) {
+		int kInf = 1_000_000;
 		// make arr2 sorted and unique
         Arrays.sort(arr2);
         List<Integer> temp2 = new ArrayList<Integer>();
@@ -52,28 +53,42 @@ public class MakeArrayStrictlyIncreasing {
         //dp
         int[] keep = new int[arr1.length];
         int[][] swap = new int[arr1.length][arr2.length];
-        Arrays.fill(keep, Integer.MAX_VALUE);
+        Arrays.fill(keep, kInf);
         keep[0] = 0;
         for(int[] arr : swap){
-        	Arrays.fill(arr, Integer.MAX_VALUE);
+        	Arrays.fill(arr, kInf);
         }
         Arrays.fill(swap[0], 1);
-        for(int i = 0; i < arr1.length; i++){
-        	int min_keap = Integer.MAX_VALUE;
-        	int min_swap = Integer.MAX_VALUE;
+        for(int i = 1; i < arr1.length; i++){
+        	int min_keep = kInf;
+        	int min_swap = kInf;
         	for(int j = 0; j < arr2.length; j++){
-        		
-        		
-        		keep[i] = Math.min(keep[i], min_keap);
+        		//case 4: b[j] > b[k] always true
+        		if(j > 0){
+        			min_swap = Math.min(min_swap, swap[i - 1][j - 1] + 1);
+        		}
+        		//case 3: a[i] > b[j]
+        		if(arr1[i] > arr2[j]){
+        			min_keep = Math.min(min_keep, swap[i - 1][j]);
+        		}
+        		//case 1: a[i] > a[i - 1]
+        		if(arr1[i] > arr1[i - 1]){
+        			keep[i] = keep[i - 1];
+        		}
+        		//case 2: b[j] > a[i - 1]
+        		if(arr2[j] > arr1[i - 1]){
+        			swap[i][j] = keep[i - 1] + 1;
+        		}
+        		keep[i] = Math.min(keep[i], min_keep);
         		swap[i][j] = Math.min(swap[i][j], min_swap);
         	}
         }
-        int resBySwap = Integer.MAX_VALUE;
+        int resBySwap = kInf;
         for(int i = 0; i < arr2.length; i++){
         	resBySwap = Math.min(resBySwap, swap[arr1.length - 1][i]);
         }
         int resByKeep = keep[arr1.length - 1];
         int res = Math.min(resByKeep, resBySwap);
-        return res < Integer.MAX_VALUE ? res : - 1;
+        return res >= kInf ? -1 : res;
     }
 }
