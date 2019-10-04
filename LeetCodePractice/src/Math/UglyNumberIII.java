ffd8ee0e922f6e4ef2916be3f75418e773881b43
tengdahe <tengdahe@192.168.1.103>
2019-10-04 10:10:55 -0700

    SmallestStringWithSwaps

--- LeetCodePractice/src/Math/UglyNumberIII.java
+++ LeetCodePractice/src/Math/UglyNumberIII.java
@@ -1,4 +1,5 @@
 package Math;
+
 /*Write a program to find the n-th ugly number.
 
 Ugly numbers are positive integers which are divisible by a or b or c.
@@ -32,7 +33,36 @@ Constraints:
 1 <= a * b * c <= 10^18
 It's guaranteed that the result will be in range [1, 2 * 10^9]
 */public class UglyNumberIII {
+	int max = 2 * 1000000000;
 	public int nthUglyNumber(int n, int a, int b, int c) {
-        
-    }
+		int start = 1, end = max, res = 0;
+		while(start <= end){
+			int mid = start + (end - start) / 2;
+			if(count(mid, a, b, c) >= n){
+				res = mid;
+				end = mid - 1;
+			} else {
+				start = mid + 1;
+			}
+		}
+		return res;
+	}
+
+	private int count(int num, int a, int b, int c) {
+		return (int) (num / a + num / b + num / c - num / lcm(a, b) - num / lcm(b, c) - num / lcm(a, c)
+				+ num / lcm(a, lcm(b, c)));
+	}
+
+	// greatest common divider
+	private long gcd(long a, long b) {
+		if (a == 0) {
+			return b;
+		}
+		return gcd(b % a, a);
+	}
+
+	// least common multiple
+	private long lcm(long a, long b) {
+		return a * b / gcd(a, b);
+	}
 }
