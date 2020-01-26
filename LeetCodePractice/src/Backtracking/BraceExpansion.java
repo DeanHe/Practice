a41a6cf36dd62146474d82b0be0740ace0ae6555
Tengda He <tengh@amazon.com>
2020-01-25 22:48:14 -0800

    MinimumNumberOfTapsToOpenToWateraGarden

---
+++ LeetCodePractice/src/Backtracking/BraceExpansion.java
@@ -0,0 +1,57 @@
+package Backtracking;
+
+import java.util.*;
+
+/*
+A string S represents a list of words.
+
+        Each letter in the word has 1 or more options. If there is one option, the letter is represented as is. If there is more than one option, then curly braces delimit the options. For example, “{a,b,c}” represents options [“a”, “b”, “c”].
+
+        For example, “{a,b,c}d{e,f}” represents the list [“ade”, “adf”, “bde”, “bdf”, “cde”, “cdf”].
+
+        Return all words that can be formed in this manner, in lexicographical order.
+
+        Example 1:
+
+        Input: "{a,b}c{d,e}f"
+        Output: ["acdf","acef","bcdf","bcef"]
+        Example 2:
+
+        Input: "abcd"
+        Output: ["abcd"]
+        Note:
+
+        1 <= S.length <= 50
+        There are no nested curly brackets.
+        All characters inside a pair of consecutive opening and ending curly brackets are different.
+*/
+public class BraceExpansion {
+    List<String> res = new ArrayList<>();
+    public String[] expand(String s) {
+        if(s == null || s.length() == 0){
+            return new String[0];
+        }
+        dfs(s, 0, "");
+        Collections.sort(res);
+        return res.toArray(new String[res.size()]);
+    }
+    private void dfs(String s, int pos, String temp){
+        if(pos == s.length()){
+            res.add(temp);
+            return;
+        }
+        char c = s.charAt(pos);
+        if(c == '{'){
+            int end = pos + 1;
+            while(s.charAt(end) != '}'){
+                end++;
+            }
+            String[] pool = s.substring(pos + 1, end).split(",");
+            for(String letter : pool){
+                dfs(s, end + 1, temp + letter);
+            }
+        } else {
+            dfs(s, pos + 1, temp + c);
+        }
+    }
+}
--- LeetCodePractice/src/Solution.java
+++ LeetCodePractice/src/Solution.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+import Backtracking.BraceExpansion;
 import UnionFind.TheEarliestMomentWhenEveryoneBecomeFriends;
 
 public class Solution {
@@ -11,10 +12,17 @@ public class Solution {
 //		ConstructBinarySearchTreeFromLevelOrderTraversals sol = new ConstructBinarySearchTreeFromLevelOrderTraversals();
 //		TreeNode root = sol.constructTree(level);
 //		System.out.println(root.val);
-		TheEarliestMomentWhenEveryoneBecomeFriends mie = new TheEarliestMomentWhenEveryoneBecomeFriends();
+
+/*		TheEarliestMomentWhenEveryoneBecomeFriends mie = new TheEarliestMomentWhenEveryoneBecomeFriends();
 		int[][] events = {{8,9,5}, {9,12,4}, {12,14,2}, {8,12,7}, {13,16,3}, {14,15,4}};
 		int[][] logs = {{20190101,0,1}, {20190104,3,4}, {20190107,2,3}, {20190211,1,5}, {20190224,2,4}, {20190301,0,3}, {20190312,1,2}, {20190322,4,5}};
-		int res = mie.earliestAcq(logs, 6);
-		System.out.println(res);
+		int res = mie.earliestAcq(logs, 6);*/
+
+		BraceExpansion be = new BraceExpansion();
+		String input = "{a,b,c}d{e,f}";
+		String[] res = be.expand(input);
+		int x = 0;
+		Arrays.stream(res).forEach(a -> System.out.println(a));
+
 	}
 }
