a9b05ea21d8dc7316f5628916d12a80a4e5bf21f
Tengda He <tengh@amazon.com>
2020-12-05 22:59:15 -0800

    canPlaceFlowers

--- LeetCodePractice/src/Greedy/CanPlaceFlowers.java
+++ LeetCodePractice/src/Greedy/CanPlaceFlowers.java
@@ -2,17 +2,17 @@ package Greedy;
 
 public class CanPlaceFlowers {
 	public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        int len = flowerbed.length;
+        int len = flowerbed.length, cnt = 0;
         for(int i = 0; i < len; i++){
             int pre = i - 1;
             int next = i + 1;
             if((pre < 0 || flowerbed[pre] == 0) && (next >= len || flowerbed[next] == 0)){
                 if(flowerbed[i] == 0){
                     flowerbed[i] = 1;
-                    n--;
+                    cnt++;
                 }
             }
-            if(n <= 0){
+            if(cnt >= n){
                 return true;
             }
         }
