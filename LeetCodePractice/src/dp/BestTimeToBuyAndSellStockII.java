f889a6114f7ee6a672fe1aa8be22ca222fe4d1a6
DeanHe <tengda.he@gmail.com>
2021-04-17 20:54:53 -0700

    BestTimeToBuyAndSellStockIII

--- LeetCodePractice/src/dp/BestTimeToBuyAndSellStockII.java
+++ LeetCodePractice/src/dp/BestTimeToBuyAndSellStockII.java
@@ -1,10 +1,12 @@
 package dp;
-/*Say you have an array for which the ith element is the price of a given stock on day i.
+/*
+Say you have an array for which the ith element is the price of a given stock on day i.
 
 Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times). However, you may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
 
 Example
-Given an example [2,1,2,0,1], return 2*/
+Given an example [2,1,2,0,1], return 2
+*/
 public class BestTimeToBuyAndSellStockII {
 	/**
      * @param prices: Given an integer array
--- LeetCodePractice/src/dp/BestTimeToBuyAndSellStockIII.java
+++ LeetCodePractice/src/dp/BestTimeToBuyAndSellStockIII.java
@@ -1,4 +1,7 @@
 package dp;
+
+import java.util.Arrays;
+
 /*
 Say you have an array for which the ith element is the price of a given stock on day i.
 
@@ -47,5 +50,31 @@ public class BestTimeToBuyAndSellStockIII {
         	res = Math.max(res, left[i] + right[i]);
         }
         return res;
+    }
+
+    public int maxProfitDP(int[] prices) {
+        int len = prices.length;
+        int[][] dp = new int[len][4];
+        /*
+        dp[i][0] means max profit gained by day i with hold first stock
+        dp[i][1] means max profit gained by day i with sold first stock
+        dp[i][2] means max profit gained by day i with hold second stock
+        dp[i][3] means max profit gained by day i with sold second stock
+         */
+        dp[0][0] = -prices[0];
+        dp[0][2] = Integer.MIN_VALUE;
+        for(int i = 1; i < len; i++){
+            dp[i][0] = Math.max(dp[i - 1][0], -prices[i]);
+            dp[i][1] = dp[i - 1][1];
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+            dp[i][2] = Math.max(dp[i - 1][2], dp[i - 1][1] - prices[i]);
+            dp[i][3] = Math.max(dp[i - 1][3], dp[i - 1][2] + prices[i]);
+        }
+        /*
+        for(int[] arr : dp){
+            System.out.println(Arrays.toString(arr));
+        }
+        */
+        return Math.max(dp[len - 1][1], dp[len - 1][3]);
     }
 }
