799e30a6abbb9d05e732fa847a24e1d437322a70
DeanHe <tengda.he@gmail.com>
2021-05-16 09:54:39 -0700

    BinaryTreeCameras

--- LeetCodePractice/src/bst/BinaryTreeCameras.java
+++ LeetCodePractice/src/bst/BinaryTreeCameras.java
@@ -1,9 +1,27 @@
 package bst;
 
-/*Given a binary tree, we install cameras on the nodes of the tree. 
+/*
+Given a binary tree, we install cameras on the nodes of the tree.
 Each camera at a node can monitor its parent, itself, and its immediate children.
-Calculate the minimum number of cameras needed to monitor all nodes of the tree.*/
-//https://leetcode.com/problems/binary-tree-cameras/discuss/211180/JavaC%2B%2BPython-Greedy-DFS
+Calculate the minimum number of cameras needed to monitor all nodes of the tree.
+
+Example 1:
+Input: [0,0,null,0,0]
+Output: 1
+Explanation: One camera is enough to monitor all nodes if placed as shown.
+
+Example 2:
+Input: [0,0,null,0,null,0,null,null,0]
+Output: 2
+Explanation: At least two cameras are needed to monitor all nodes of the tree. The above image shows one of the valid configurations of camera placement.
+
+Note:
+The number of nodes in the given tree will be in the range [1, 1000].
+Every node has value 0.
+
+https://leetcode.com/problems/binary-tree-cameras/discuss/211180/JavaC%2B%2BPython-Greedy-DFS
+*/
+
 public class BinaryTreeCameras {
 	int camera = 0;
     public int minCameraCover(TreeNode root) {
