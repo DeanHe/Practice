62c31367a549df219b1280acea514cbebccf7cef
tengda <tengda.he@gmail.com>
2021-07-23 00:20:47 -0700

    SubarraysWithKDifferentIntegers

--- LeetCodePractice/src/twoPointers/window/SubarraysWithKDifferentIntegers.java
+++ LeetCodePractice/src/twoPointers/window/SubarraysWithKDifferentIntegers.java
@@ -4,22 +4,33 @@ import java.util.HashMap;
 import java.util.Map;
 
 /*
-Given an array A of positive integers, call a (contiguous, not necessarily distinct) subarray of A good if the number of different integers in that subarray is exactly K.
+Given an array nums of positive integers, call a (contiguous, not necessarily distinct) subarray of nums good if the number of different integers in that subarray is exactly k.
 
 (For example, [1,2,3,1,2] has 3 different integers: 1, 2, and 3.)
 
-Return the number of good subarrays of A.
+Return the number of good subarrays of nums.
+
+
 
 Example 1:
-Input: A = [1,2,1,2,3], K = 2
+
+Input: nums = [1,2,1,2,3], k = 2
 Output: 7
 Explanation: Subarrays formed with exactly 2 different integers: [1,2], [2,1], [1,2], [2,3], [1,2,1], [2,1,2], [1,2,1,2].
-
 Example 2:
-Input: A = [1,2,1,3,4], K = 3
+
+Input: nums = [1,2,1,3,4], k = 3
 Output: 3
 Explanation: Subarrays formed with exactly 3 different integers: [1,2,1,3], [2,1,3], [1,3,4].
 
+
+Note:
+
+1 <= nums.length <= 20000
+1 <= nums[i] <= nums.length
+1 <= k <= nums.length
+
+analysis:
 Time Complexity: O(n), where n is the length of A.
 Space Complexity: O(n).
 */
