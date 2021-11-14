31e2d2ba7f0ca7a8fa6032b903b9c9d4884af12e
DeanHe <tengda.he@gmail.com>
2021-11-14 10:19:00 -0800

    MinimumStringLengthAfterTwoDeletion

---
+++ LeetCodePractice/src/string/MinimumStringLengthAfterTwoDeletion.java
@@ -0,0 +1,29 @@
+package string;
+/*
+Given a string, you can apply deletion two times,
+and the deletion rule is that can only remove concatenated same characters
+find the shortest string length after operations
+
+ex:
+1
+input aabcccbb
+delete ccc
+delete bbb
+remain aa
+return 2
+
+2
+aabaa
+delete b
+delete aaaa
+remain ""
+return 0
+ */
+public class MinimumStringLengthAfterTwoDeletion {
+    public int minimumLengthAfterDeletion(String str){
+        int res = 0;
+        for(){
+
+        }
+    }
+}
